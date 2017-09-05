66b40694e0a1a300d94e875362bbdd51e3f89b91
Han Chang <szu.han.chang@gmail.com>
2017-09-04 22:38:02 -0500

    Upgrade to show volume in chart

--- app/assets/javascripts/coins.js
+++ app/assets/javascripts/coins.js
@@ -1,5 +1,21 @@
 $(function() {
   $.getJSON('/historical/btc', function (data) {
+    var prices = [],
+        volume = [],
+        dataLength = data.length,
+        i = 0;
+
+    for (i; i < dataLength; i += 1) {
+      prices.push([
+        data[i][0], // timestamp
+        data[i][1], // price
+      ]);
+      volume.push([
+        data[i][0], // timestamp
+        data[i][2], // volume
+      ]);
+    }
+
     Highcharts.stockChart('chart', {
       rangeSelector: {
         selected: 1
@@ -9,9 +25,42 @@ $(function() {
         text: 'Bitcoin Price'
       },
 
+      yAxis: [{
+        labels: {
+          align: 'right',
+          x: -3
+        },
+        title: {
+          text: 'Price'
+        },
+        height: '60%',
+        lineWidth: 2
+      }, {
+        labels: {
+          align: 'right',
+          x: -3
+        },
+        title: {
+          text: 'Volume'
+        },
+        top: '65%',
+        height: '35%',
+        offset: 0,
+        lineWidth: 2
+      }],
+
+      tooltip: {
+        split: true
+      },
+
       series: [{
         name: 'BTC',
-        data: data
+        data: prices,
+      }, {
+        type: 'column',
+        name: 'Volume',
+        data: volume,
+        yAxis: 1
       }]
     });
   });
--- app/models/daily_price.rb
+++ app/models/daily_price.rb
@@ -2,6 +2,10 @@ class DailyPrice < ApplicationRecord
   belongs_to :coin
 
   scope :historical, ->(coin_id, currency) {
-    where(coin_id: coin_id).order(:date).pluck(:timestamp, "#{currency}_price".to_sym)
+    where(coin_id: coin_id).order(:date).pluck(
+      :timestamp,
+      "#{currency}_price".to_sym,
+      "#{currency}_volume".to_sym
+    )
   }
 end
