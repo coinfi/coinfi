8df849f3b4438ac8078f89a36290af47725f498c
Han Chang <szu.han.chang@gmail.com>
2017-09-10 10:36:59 -0500

    Customize look and feel

--- app/assets/javascripts/coins.js
+++ app/assets/javascripts/coins.js
@@ -23,7 +23,42 @@ $(function() {
     Highcharts.setOptions({
       lang: {
         thousandsSep: ','
-      }
+      },
+      // http://jkunst.com/highcharts-themes-collection/
+      // https://raw.githubusercontent.com/jbkunst/highcharts-themes-collection/gh-pages/themes/google.js
+      "colors": [
+        "#0266C8",
+        "#F90101",
+        "#F2B50F",
+        "#00933B"
+      ],
+      "chart": {
+        "style": {
+          "fontFamily": "Roboto",
+          "color": "#444444"
+        }
+      },
+      "xAxis": {
+        "gridLineWidth": 1,
+        "gridLineColor": "#F3F3F3",
+        "lineColor": "#F3F3F3",
+        "minorGridLineColor": "#F3F3F3",
+        "tickColor": "#F3F3F3",
+        "tickWidth": 1
+      },
+      "yAxis": {
+        "gridLineColor": "#F3F3F3",
+        "lineColor": "#F3F3F3",
+        "minorGridLineColor": "#F3F3F3",
+        "tickColor": "#F3F3F3",
+        "tickWidth": 1
+      },
+      "legendBackgroundColor": "rgba(0, 0, 0, 0.5)",
+      "background2": "#505053",
+      "dataLabelsColor": "#B0B0B3",
+      "textColor": "#C0C0C0",
+      "contrastTextColor": "#F0F0F3",
+      "maskColor": "rgba(255,255,255,0.3)"
     });
 
     Highcharts.stockChart('chart', {
