f33cb0abe4abc6cb4e03cee6b14e34b5c92fd926
Han Chang <szu.han.chang@gmail.com>
2017-09-24 18:29:50 -0700

    Add whale detection page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
   RSS_URL = 'http://admin.coinfi.com/rss/'.freeze
+  layout 'gsdk', only: [
+    :home,
+    :detect_whale_price_manipulation,
+    :predict_coins,
+    :find_relevant_news,
+    :cloak_trading,
+    :identify_best_priced_exchange,
+    :research_bad_actors,
+    :facilitate_altcoin_coverage
+  ]
 
   def home
-    render layout: false
   end
 
   def about
@@ -18,5 +27,26 @@ class PagesController < ApplicationController
     entry = feed.entries.first
     @title = entry.title
     @content = entry.content.html_safe
+  end
+
+  def detect_whale_price_manipulation
+  end
+
+  def predict_coins
+  end
+
+  def find_relevant_news
+  end
+
+  def cloak_trading
+  end
+
+  def identify_best_priced_exchange
+  end
+
+  def research_bad_actors
+  end
+
+  def facilitate_altcoin_coverage
   end
 end
