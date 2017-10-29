9369f8cd81da9dfa6294c4076060e1ddb1dc5c13
Han Chang <szu.han.chang@gmail.com>
2017-10-29 17:50:06 -0500

    Clean up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,9 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 
