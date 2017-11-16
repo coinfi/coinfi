83753b92d867d12247aefff7baa57b745e27b035
Han Chang <szu.han.chang@gmail.com>
2017-11-16 10:57:38 -0600

    Disable Rollbar in dev env

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,8 +4,8 @@ Rollbar.configure do |config|
 
   config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN')
 
-  # Here we'll disable in 'test':
-  if Rails.env.test?
+  # Here we'll disable in 'test' and 'development' environments:
+  if Rails.env.test? || Rails.env.development?
     config.enabled = false
   end
 
