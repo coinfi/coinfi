5d165ea175a71a011feb09f6bdecaddea658d452
Han Chang <szu.han.chang@gmail.com>
2017-11-20 13:59:36 -0600

    Initial Devise look and feel refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "gsdk"
+    else
+      "application"
+    end
+  end
 end
