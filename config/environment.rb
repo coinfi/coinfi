add9171237f922845f17f43c938347dbab516c4e
Han Chang <szu.han.chang@gmail.com>
2017-11-20 16:14:02 -0600

    Set up Devise mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => 'smtp.webfaction.com', #ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => 'coinfi', #ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV.fetch('WEBFACTION_SMTP_PASSWORD'), #ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => ENV.fetch('ROOT_DOMAIN'),
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
