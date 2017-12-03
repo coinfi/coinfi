8af893fda9ef9e1f8bff6d610dbcca10338631fc
Han Chang <szu.han.chang@gmail.com>
2017-12-02 22:09:08 -0600

    Eager load lib paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module CoinfiRails
     config.load_defaults 5.1
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
