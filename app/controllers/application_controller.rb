a2a682f6500c857269e1eaac22f6b4b1ad994dd5
Han Chang <szu.han.chang@gmail.com>
2017-12-11 17:11:29 +0800

    Redirect to dashboard after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
   private
 
   def layout_by_resource
