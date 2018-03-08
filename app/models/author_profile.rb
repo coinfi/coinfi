d14c978b439fbae2d70430297f012d8013a3c536
jake <jake@mythical.fish>
2018-03-08 20:23:00 +0800

    Remove Author Profile validation except for name

--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -7,9 +7,6 @@ class AuthorProfile < ApplicationRecord
   accepts_nested_attributes_for :user
 
   validates :name, presence: true, length: { minimum: 3 }
-  validates :company, presence: true, length: { minimum: 3 }
-  validates :role, presence: true, length: { minimum: 3 }
-  validates :bio, presence: true
 
   def username
     user.username
