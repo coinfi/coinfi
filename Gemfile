e7216b641ce853b9cece5b57d3b11aa53e11f619
Sam Peacey <sam.peacey@gmail.com>
2018-03-10 16:50:58 +0900

    updated rails to 5.1.5, system tests working

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.3'
+gem 'rails', '~> 5.1.5'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
@@ -67,7 +67,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '~> 2.13'
+  gem 'capybara', '~> 2.15'
   gem 'chromedriver-helper'
   gem 'dotenv-rails'
   gem 'factory_bot_rails'
--- test/system/contributor_submissions_test.rb
+++ test/system/contributor_submissions_test.rb
@@ -4,8 +4,6 @@ class ContributorSubmissionsTest < ApplicationSystemTestCase
   test "can submit content" do
     visit root_path
     user = FactoryBot.create(:user)
-    pp User.all
-    pp user.valid_password?('password')
     sign_in(user)
 
     click_link 'Contribute Content'
@@ -18,7 +16,6 @@ class ContributorSubmissionsTest < ApplicationSystemTestCase
     assert_content 'successfully created'
     assert_content 'Content Title'
 
-    pp ContributorSubmission.count
     submission = ContributorSubmission.last
     visit contributor_submission_path(submission)
 
@@ -26,9 +23,4 @@ class ContributorSubmissionsTest < ApplicationSystemTestCase
     assert_content 'Content Disclosure'
     assert_content 'This Is The Content'
   end
-  # test "visiting the index" do
-  #   visit contributor_submissions_url
-  #
-  #   assert_selector "h1", text: "ContributorSubmission"
-  # end
 end
