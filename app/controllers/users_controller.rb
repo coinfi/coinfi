860e8f4d074495dcd03d9c126ad4980f7a150249
Han <han@hanchang.co>
2018-03-09 23:20:50 -0600

    Remove KYC logic (#20)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 require 'open-uri'
-BLOCKED_COUNTRIES = ENV.fetch('BLOCKED_COUNTRIES').to_s.split(',')
 
 class UsersController < DeviseController
   layout 'gsdk'
   before_action :check_user_signed_in, except: [:signup]
-  before_action :set_s3_direct_post, only: [:kyc, :submit_kyc]
 
   def signup
     @email = params[:email] || user_params[:email]
@@ -51,90 +49,13 @@ class UsersController < DeviseController
     end
   end
 
-  def estimate_contribution
-  end
-
-  def submit_contribution
-    current_user.token_sale = {} if current_user.token_sale.blank?
-    current_user.token_sale['estimated_contribution'] = user_params[:estimated_contribution]
-    if current_user.save
-      redirect_to '/join-telegram'
-    else
-      redirect_to '/estimate-contribution', notice: 'There was a problem saving your estimated contribution: #{current_user.errors.full_messages} - please try again.'
-    end
-  end
-
   def join_telegram
   end
 
   def dashboard
   end
 
-  def kyc
-    render_if_blocked_country
-    redirect_to dashboard_path if current_user.kyc_completed?
-  end
-
-  def submit_kyc
-    # Server side param check
-    required_params = %i[
-      first_name last_name date_of_birth gender nationality residency ethereum_address id_doc_image selfie_image
-      confirm_correct_info confirm_no_legal_restrictions confirm_privacy_policy confirm_token_agreement confirm_terms_conditions
-    ]
-    unless required_params.all? { |k| params[k].present? }
-      return redirect_to kyc_path, notice: 'You must fill in all fields, including the check boxes!'
-    end
-
-    current_user.token_sale = {} if current_user.token_sale.blank?
-    current_user.token_sale.merge!({
-      "first_name" => params[:first_name].try(:to_ascii),
-      "last_name" => params[:last_name].try(:to_ascii),
-      "date_of_birth" => params[:date_of_birth],
-      "gender" => params[:gender],
-      "nationality" => params[:nationality],
-      "residency" => params[:residency],
-      "ethereum_address" => params[:ethereum_address],
-      "id_doc_image" => params[:id_doc_image],
-      "selfie_image" => params[:selfie_image],
-      "confirm_correct_info" => params[:confirm_correct_info],
-      "confirm_no_legal_restrictions" => params[:confirm_no_legal_restrictions],
-      "confirm_privacy_policy" => params[:confirm_privacy_policy],
-      "confirm_token_agreement" => params[:confirm_token_agreement],
-      "confirm_terms_conditions" => params[:confirm_terms_conditions],
-    })
-    current_user.save
-
-    if Artemis.restricted_residencies.include? params[:residency]
-      redirect_to dashboard_path and return
-    end
-
-    # TODO: Run in background job?
-    current_user.run_kyc!
-=begin
-    if current_user.kyc_result_cleared?
-      ApplicationMailer.kyc_confirmation(current_user).deliver_later
-    end
-=end
-
-    redirect_to dashboard_path
-  end
-
-  def update_ethereum_address
-    current_user.token_sale['ethereum_address'] = params[:ethereum_address]
-    if current_user.save
-      flash[:success] = "Your Ethereum address has been successfully updated."
-    else
-      flash[:danger] = "There was an error updating your Ethereum address, please try again or reach us at contact@coinfi.com if this persists."
-    end
-
-    redirect_to dashboard_path
-  end
-
 protected
-
-  def render_if_blocked_country
-    render "blocked" if BLOCKED_COUNTRIES.include? request.env['HTTP_CF_IPCOUNTRY']
-  end
 
   def check_user_signed_in
     redirect_to new_user_session_path, notice: 'Please sign in or register.' and return unless current_user
@@ -144,12 +65,4 @@ protected
     params.require(:user).permit(:email, :password, :estimated_contribution)
   end
 
-private
-
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(
-      key: "#{current_user.id}/${filename}",
-      success_action_status: '201',
-    )
-  end
 end
