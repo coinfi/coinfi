bce22c1c6ec99ca7471f8272c753442ac8fc5c50
Jake Broughton <jake@mythical.fish>
2018-03-24 13:48:32 +0700

    [PLA-29] Extract marketing site (#21)
    * Remove sales pages & routes from platform
    
    - Change root route to dashboard
    - Removed routes
    - Removed controller (preserving the RSS action)
    - Removed views & application layout
    - Renamed gsdk layout to application & made it global
    
    * Remove Coins controller, views & routes
    
    * Remove cn page routes
    
    * Restore Coins
    
    * Add homepage & styling updates
    
    * Don't append locale param for default locale
    
    * Homepage updates
    
    * Update favicon
    
    * Ignore yarn.lock on Heroku
    
    * Add webpacker gem for installing Yarn on Heroku
    
    * Remove webpacker
    
    * Add ethereum-machine.png, styling updates
    
    * Make login button green
    
    * Homepage styling updates
    
    * Restore coins.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  mount Blazer::Engine, at: "blazer"
+
 
   devise_scope :user do
+    # TODO: (possibly)
+    # I don't think anything but devise remappings should be 
+    # within devise_scope, eg. login, signup, etc, and most 
+    # of the UsersController doesn't need to inherit from
+    # DeviseController. But I might be missing something. :)
     match "/signup" => "users#signup", as: "new_user_signup", via: [:get, :post]
-    get "/register" => "users/registrations#new", as: "new_user_registration"
+    get "/register" => "users/registrations#new", as: "register"
     get "/set-password" => "users#set_password", as: "new_user_set_password"
     post "/submit-password" => "users#submit_password", as: "new_user_submit_password"
     get "/join-telegram" => "users#join_telegram", as: "new_user_join_telegram"
-    get "/dashboard" => "users#dashboard", as: "dashboard"
+    get "/kyc", to: "users#kyc", as: "kyc"
+    post "/kyc", to: "users#submit_kyc", as: "submit_kyc"
+    post "/eth", to: "users#update_ethereum_address", as: "update_eth"
   end
 
   devise_for :users,
@@ -29,21 +36,6 @@ Rails.application.routes.draw do
     root to: "coins#index"
   end
 
-  root 'pages#home'
-
-  get '/' => 'pages#home', as: 'home'
-  get '/prototype' => 'pages#prototype', as: 'prototype'
-  get '/press-release' => 'pages#press'
-  get '/about' => 'pages#about'
-  get '/contact' => 'pages#contact'
-  get '/daily' => 'pages#daily', as: 'daily'
-  get '/identify-whale-price-manipulation' => 'pages#identify_whale_price_manipulation'
-  get '/detect-coins-about-to-moon' => 'pages#detect_coins_about_to_moon'
-  get '/find-relevant-news' => 'pages#find_relevant_news'
-  get '/display-research-pieces' => 'pages#display_research_pieces'
-  get '/cloak-trading' => 'pages#cloak_trading'
-  get '/identify-best-priced-exchange' => 'pages#identify_best_priced_exchange'
-
   resources :coins, only: [:index, :show]
 
   resources :contributor_submissions, path: 'contributor-submissions'
@@ -53,7 +45,9 @@ Rails.application.routes.draw do
 
   get '/historical/:symbol' => 'data#historical'
 
-  scope "(:locale)", locale: /en|cn/ do
-    get '/' => 'pages#home'
-  end
+  root to: 'pages#show'
+  get '/:id', to: 'pages#show'
+
+  mount Blazer::Engine, at: "blazer"
+
 end
