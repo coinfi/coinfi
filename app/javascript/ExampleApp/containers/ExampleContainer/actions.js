427ebadae5af867f12659a1779fb9b321f8b8613
Han Chang <szu.han.chang@gmail.com>
2018-04-02 16:33:47 +0800

    Remove js ExampleApp

--- app/javascript/ExampleApp/containers/ExampleContainer/actions.js
+++
@@ -1,7 +0,0 @@
-export const fetchEntity = id => {
-  return { type: 'FETCH_ENTITY', id }
-}
-
-export const fetchEntitySuccess = response => {
-  return { type: 'FETCH_ENTITY_SUCCESS', response }
-}
