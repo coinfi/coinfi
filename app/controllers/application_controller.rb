4346e9a44cedfcbc710891c3a99bbff0e3174cae
Han Chang <szu.han.chang@gmail.com>
2018-04-05 00:17:31 +0800

    Hide subheader, modify top nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,26 @@
 class ApplicationController < ActionController::Base
-
   include Responses
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :set_hide_subheader, :set_locale
 
   def after_sign_in_path_for(resource)
     '/dashboard'
   end
 
-  private
+protected
+
+  def set_hide_subheader
+    @hide_subheader = true
+  end
+
+private
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options
-    return {} if I18n.locale == I18n.default_locale 
+    return {} if I18n.locale == I18n.default_locale
     { locale: I18n.locale }
   end
 end
