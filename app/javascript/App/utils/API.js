20acb22d0619f89baecac5586d84f5ceee2051f9
jake <jake@mythical.fish>
2018-04-04 11:41:07 +0700

    Fix coin search

--- app/javascript/App/utils/API.js
+++ app/javascript/App/utils/API.js
@@ -1,37 +1,38 @@
 import axios from 'axios'
+import qs from 'qs'
+
+axios.defaults.paramsSerializer = params => {
+  return qs.stringify(params, { arrayFormat: 'brackets' })
+}
 
 const request = (path, data = {}, remote = true, type = 'get') => {
   let config = {}
   let endpoint = '/api'
-  if (remote) {
-    endpoint = process.env['COINFI_PRICES_URL']
-  } else {
-    config = {
-      headers: {
-        'X-CSRF-Token': document
-          .querySelector('meta[name="csrf-token"]')
-          .getAttribute('content')
-      }
-    }
+  let params = data
+  const headers = {
+    'X-CSRF-Token': document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute('content')
   }
+  if (type === 'get') params = { params }
+  if (remote) endpoint = process.env['COINFI_PRICES_URL']
+  if (!remote) config = { headers }
   const url = `${endpoint}${path}`
   return new Promise(resolve => {
     if (type === 'delete') {
       axios
-        .delete(url, { data, headers: config.headers })
+        .delete(url, { data, headers })
         .then(response => {
-          const d = remote ? response : response.data
-          resolve(d)
+          resolve(response.data || response)
         })
         .catch(error => {
           console.log(error)
         })
       return
     }
-    axios[type](url, data, config)
+    axios[type](url, params, config)
       .then(response => {
-        const d = remote ? response : response.data
-        resolve(d)
+        resolve(response.data || response)
       })
       .catch(error => {
         console.log(error)
