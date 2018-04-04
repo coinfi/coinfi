b0d4fa7b2001cb706a2dbcbfb0f5a00cc8d9b2f9
jake <jake@mythical.fish>
2018-04-04 11:41:18 +0700

    Styling updates

--- app/javascript/App/components/WatchlistItem.js
+++ app/javascript/App/components/WatchlistItem.js
@@ -6,14 +6,16 @@ export default ({ coin }) => {
   const { name, image_url, symbol, market_info: info, category } = coin
   return (
     <div className="bg-white shadow-s1 ba b--athens-darker mb3">
-      <div className="flex items-center justify-between pa3">
-        {image_url && (
-          <img className="w2e h2e mr3 a1" src={image_url} alt={name} />
-        )}
-        <h1 className="ma0 lh-solid f3 flex-auto">
-          <div className="f6 fw9 o-50">{symbol}</div>
-          <div className="">{name}</div>
-        </h1>
+      <div className="flex items-end justify-between pa4 pb3">
+        <div className="flex items-center">
+          {image_url && (
+            <img className="w3e h3e mr3" src={image_url} alt={name} />
+          )}
+          <h1 className="ma0 lh-solid f3 flex-auto">
+            <div className="f6 fw9 o-50 mb1">{symbol}</div>
+            <div className="">{name}</div>
+          </h1>
+        </div>
         <div className="f4 fw9 tr">
           <PercentageChange
             number={info.percent_change_24h}
@@ -23,7 +25,7 @@ export default ({ coin }) => {
         </div>
       </div>
       {category === 'listed' && (
-        <div className="bt b--athens-dark pa3">
+        <div className="bt b--athens-dark pa4 pt3">
           <div className="row mtn4 tr o-90">
             <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
               <label className="o-60">Volume</label>
