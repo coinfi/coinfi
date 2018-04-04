59eae3f819591fbfa96754fc8a9883fc51e13905
Han Chang <szu.han.chang@gmail.com>
2018-04-04 23:01:31 +0800

    Tweak ICO layout in watchlist

--- app/javascript/App/components/WatchlistItem.js
+++ app/javascript/App/components/WatchlistItem.js
@@ -17,13 +17,23 @@ export default props => {
           </h1>
         </div>
         <div className="f4 fw9 tr">
-          {category === 'listed' && (
+          {category === 'listed' ? (
             <Fragment>
               <PercentageChange
                 number={info.percent_change_24h}
                 className="smaller2 b mr2"
               />
               <Currency>{info.price_usd}</Currency>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <div className="green">
+                <Currency>{coin.ico_usd_raised || 0}</Currency>
+              </div>
+              <div className="mh2 f7 ttu silver">of</div>
+              <div>
+                <Currency>{coin.ico_fundraising_goal_usd || 0}</Currency>
+              </div>
             </Fragment>
           )}
         </div>
@@ -42,25 +52,35 @@ export default props => {
               <div className="f7 ml1">{symbol}</div>
             </div>
             <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
-              <label className="o-60">Market cap</label>
+              <label className="o-60">Market Cap</label>
               {info.market_cap_usd}
               <div className="f7 ml1">USD</div>
             </div>
             <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
-              <label className="o-60">Total supply</label>
+              <label className="o-60">Total Supply</label>
               {info.total_supply}
               <div className="f7 ml1">{symbol}</div>
             </div>
           </div>
         ) : (
-          <div className="flex items-center sans-alt fw6">
-            <div className="mr2">Raised</div>
-            <div className="green">
-              <Currency>{coin.ico_usd_raised || 0}</Currency>
+          <div className="row mtn4 tr o-90">
+            <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
+              <label className="o-60">Total Supply</label>
+              {coin.max_supply}
+              <div className="f7 ml1">{symbol}</div>
             </div>
-            <div className="mh2 f7 ttu silver">of</div>
-            <div>
-              <Currency>{coin.ico_fundraising_goal_usd || 0}</Currency>
+            <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
+              <label className="o-60">ICO Price</label>
+              {coin.ico_token_price_usd}
+              <div className="f7 ml1">USD</div>
+            </div>
+            <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
+              <label className="o-60">Start Date</label>
+              {coin.ico_start_date}
+            </div>
+            <div className="stat-block col-xs-6 col-sm-3 col-md-12 col-lg-3 mt4">
+              <label className="o-60">End Date</label>
+              {coin.ico_end_date}
             </div>
           </div>
         )}
