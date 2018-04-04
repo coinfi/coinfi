67ccbf1a374e6e89199184ff81796c4b56410b4b
jake <jake@mythical.fish>
2018-04-04 20:35:45 +0700

    Search styling updates

--- app/javascript/App/components/WatchlistSearch.js
+++ app/javascript/App/components/WatchlistSearch.js
@@ -8,23 +8,25 @@ export default class WatchlistSearch extends Component {
   render() {
     const { searchedCoins, addCoinSuccess, searchText } = this.props
     return (
-      <div>
-        <div className="bg-white">
-          <div className="pa3">
-            <input
-              type="text"
-              value={searchText}
-              onChange={this.handleSearchInput}
-              className="input-alt tc"
-              placeholder="Search"
-            />
-          </div>
-        </div>
+      <div className="bg-white pa3">
+        <input
+          type="text"
+          value={searchText}
+          onChange={this.handleSearchInput}
+          className="input-alt tc"
+          placeholder="Search"
+        />
         {searchedCoins.size > 0 && (
-          <div>
+          <div className="mt3">
             {searchedCoins.map(coin => (
-              <div key={coin.get('id')} className="pb3 mb3 bb b--athens-dark">
-                {coin.get('name')}
+              <div
+                key={coin.get('id')}
+                className="pb3 mb3 bb b--athens-dark flex justify-between"
+              >
+                <div>
+                  {coin.get('name')}
+                  <span className="b ml2 f7">{coin.get('symbol')}</span>
+                </div>
                 <WatchButton
                   coinID={coin.get('id')}
                   noFetch
