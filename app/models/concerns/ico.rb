8f00aec2fe57e92b96a2def7df3f489b154088f3
Sam Peacey <sam.peacey@gmail.com>
2018-04-07 01:15:21 +0900

    use ico_listed? method instead of historical_data_exists? to determine if coin is listed

--- app/models/concerns/ico.rb
+++ app/models/concerns/ico.rb
@@ -2,7 +2,7 @@ module ICO
   extend ActiveSupport::Concern
 
   included do
-    alias_method "is_listed?", "historical_data_exists?"
+    alias_method "is_listed?", "ico_listed?"
   end
 
   def category
@@ -11,18 +11,6 @@ module ICO
 
   def is_ico?
     !is_listed?
-  end
-
-  def historical_data_exists?
-    Rails.cache.fetch(
-      "/coins/#{symbol}/historical_data_exists?",
-      { expires_in: 1.day }
-    ) do
-      url = "#{ENV.fetch('COINFI_PRICES_URL')}" <<
-        "api/v1/coins/#{symbol}/daily_history.json"
-      response = Net::HTTP.get_response(URI.parse(url))
-      response.kind_of? Net::HTTPSuccess
-    end
   end
 
   # TODO: Refactor as a helper
