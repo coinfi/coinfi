f79abfcc140100d78816949242d6ca7fd473e83b
jake <jake@mythical.fish>
2018-04-06 18:44:30 +0700

    Filter styling updates

--- app/javascript/App/components/ArticleFilters.js
+++ app/javascript/App/components/ArticleFilters.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export default () => (
-  <div className="o-40 black-80 relative noselect">
-    <div className="absolute absolute--fill hide-child">
-      <div className="black b absolute absolute--fill pt1 bg-white-60 child tc">
+  <div className="black-80 relative noselect">
+    <div className="absolute absolute--fill">
+      <div className="b absolute absolute--fill pt2 bg-white-90 ttu tc black-50 f6">
         Coming soon
       </div>
     </div>
-    <div>
+    <div className="o-20">
       <div className="dib ttu pa2 ph3 f7 ba b--moon-gray sans-alt">
         Tags
         <i className="fas fa-chevron-down ml2" />
