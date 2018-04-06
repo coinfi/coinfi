6e05e61ecef0ea13805ec794e00baf2fd2624b8e
Sam Peacey <sam.peacey@gmail.com>
2018-04-06 11:33:14 +0900

    changed review date from date to datetime

--- db/migrate/20180406014547_add_review_date_to_influencer_reviews.rb
+++ db/migrate/20180406014547_add_review_date_to_influencer_reviews.rb
@@ -1,5 +1,5 @@
 class AddReviewDateToInfluencerReviews < ActiveRecord::Migration[5.1]
   def change
-    add_column :influencer_reviews, :review_date, :date
+    add_column :influencer_reviews, :review_date, :datetime
   end
 end
