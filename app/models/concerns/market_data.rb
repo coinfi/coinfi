c09aaf7181e83521ea3948656b3eec3928e8f747
Sam Peacey <sam.peacey@gmail.com>
2018-04-08 14:23:26 +0900

    use https endpoint for API call, remove comment

--- app/models/concerns/market_data.rb
+++ app/models/concerns/market_data.rb
@@ -9,7 +9,7 @@ module MarketData
       "/coins/#{symbol}/market_data",
       { expires_in: 1.minute }
     ) do
-      url = "http://coinmarketcap.northpole.ro/ticker.json?identifier=#{slug}&version=v8"
+      url = "https://coinmarketcap.northpole.ro/ticker.json?identifier=#{slug}&version=v8"
       response = HTTParty.get(url)
       data = JSON.parse(response.body) || {}
       default_market_data.merge(transform_northpole_data(data))
@@ -42,7 +42,7 @@ module MarketData
 
   def transform_northpole_data(data)
     {
-      '24h_volume_usd' => data.dig('markets', 0, 'volume24', 'usd'),    # TODO: replace with `dig`?
+      '24h_volume_usd' => data.dig('markets', 0, 'volume24', 'usd'),
       'available_supply' => data.dig('markets', 0, 'availableSupply'),
       'market_cap_usd' => data.dig('markets', 0, 'marketCap', 'usd'),
       'price_usd' => data.dig('markets', 0, 'price', 'usd')
