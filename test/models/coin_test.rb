7c73ba57ced7d70fb2cac6b42fa7677ff7e58edc
Sam Peacey <sam.peacey@gmail.com>
2018-04-14 20:26:05 +0900

    update previous name when name changes (#62)

---
+++ test/models/coin_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CoinTest < ActiveSupport::TestCase
+  def setup
+    @coin = Coin.create(name: 'CoinName')
+  end
+
+  test 'updates previous_name to name if name changes' do
+    @coin.update_attribute :name, 'NewCoinName'
+    assert_equal 'CoinName', @coin.previous_name
+  end
+
+  test "does not change previous_name if name doesn't change" do
+    @coin.update_attribute :symbol, 'CN'
+    assert_equal nil, @coin.previous_name
+  end
+end
