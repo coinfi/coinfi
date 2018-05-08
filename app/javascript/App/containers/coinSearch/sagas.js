12acb503d95dfb11d73820679c73ef3f33748519
jake <jake@mythical.fish>
2018-05-08 11:16:03 +0200

    Also search for ticker symbol in global coin search

--- app/javascript/App/containers/coinSearch/sagas.js
+++ app/javascript/App/containers/coinSearch/sagas.js
@@ -10,7 +10,7 @@ function* searchCoins({ searchText, searchOpts, namespace }) {
   if (searchText.length < 2) return
   yield sagas.get(
     '/coins.json',
-    Object.assign({ q: { name_cont: searchText } }, searchOpts),
+    Object.assign({ q: { search: searchText } }, searchOpts),
     actions.searchCoinsSuccess(namespace)
   )
 }
