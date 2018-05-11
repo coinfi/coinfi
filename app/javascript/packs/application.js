66fa8b22a62f4db9148a1feaf586f65adf63d611
jake <jake@mythical.fish>
2018-05-11 13:58:44 +0200

    Use React for tab navigation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import 'jquery' // jQuery has been configured to be globally available
 import '../modules/coins/chart'
 import '../modules/coins/trading-view'
 import '../modules/navigation/mobile-nav'
-import '../modules/navigation/tabs'
 
 // React
 import '../App'
