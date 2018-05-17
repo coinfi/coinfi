334a31c8f85541f52e19d2cf43cfccccc9a0e936
Gordon DuQuesnay <gduquesnay@gmail.com>
2018-05-17 10:54:00 +0000

    Create admin section for news_items + As an editor? I want to tag a news item with the coins that it refers to/mentions/affects?

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,10 @@
 class NewsItem < ApplicationRecord
   belongs_to :feed_source
   has_one :news_item_raw
+  has_many :mentions
+  has_many :coins, through: :mentions
+
+  def coin_symbols
+    coins.pluck(:symbol).join(', ')
+  end
 end
