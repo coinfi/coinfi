ff2d12c6c93bc92f6d9d7dcc0d66730229345600
Sam Peacey <sam.peacey@gmail.com>
2018-05-18 12:22:37 +0900

    nowrap to edit watchlist buttons (#85)

--- app/javascript/App/components/WatchlistItem/Actions.js
+++ app/javascript/App/components/WatchlistItem/Actions.js
@@ -6,13 +6,13 @@ export default SortableHandle(props => {
   return (
     <div className="flex justify-end">
       <button
-        className="btn btn-xs btn-white mr2"
+        className="btn btn-xs btn-white mr2 no-wrap"
         onClick={() => removeCoin(coin.id)}
       >
         <i className="fal fa-trash mr2" />
         Remove
       </button>
-      <div className="btn btn-xs btn-white cursor-move">
+      <div className="btn btn-xs btn-white cursor-move no-wrap">
         <i className="fal fa-arrows-alt mr2" />
         Reposition
       </div>
