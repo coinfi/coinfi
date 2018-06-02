b5a7b4cbf3a5985ad3ec8fb0895547c1502229b9
Han Chang <szu.han.chang@gmail.com>
2018-06-02 09:48:41 +0800

    Clean up CoinFi News Tips

--- app/javascript/App/components/NewsfeedPage/Tips.js
+++ app/javascript/App/components/NewsfeedPage/Tips.js
@@ -4,25 +4,17 @@ export default () => {
   return (
     <div className="pa5 lh-copy">
       <div>
-        CoinFi News gives Crypto investors like you an information advantage by
-        filtering out the noise and showing you how news in impacting coin
+        CoinFi News gives crypto investors like you an informational advantage by
+        filtering out the noise and showing you how news is impacting coin
         price.
       </div>
       <h2>Tips</h2>
       <ul>
-        <li>Click coin to see its details and associated articles.</li>
-        <li>Click an article to see its details.</li>
-        <li>
-          Use filters to drill down on specific sources or categories and coins.
-        </li>
-        <li>
-          See how news historically impacts a coin's price using CoinFi's Price
-          Chart news annotations.
-        </li>
-        <li>
-          Enable email and browser notifications to stay up-to-date on the
-          latest market moving news.
-        </li>
+        <li>Add coins to your watchlist to customize your news feed.</li>
+        <li>Use filters to drill down on specific news categories, types, and sources.</li>
+        <li>Search or click on a coin ticker to view coin-specific news and coin data.</li>
+        <li>See how news historically impacts a coin's price using CoinFi's Price Chart news annotations.</li>
+        <li>Enable email and browser notifications to stay up-to-date on the latest market moving news.</li>
       </ul>
     </div>
   )
