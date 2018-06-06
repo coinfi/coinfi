10b6684f189f0bbcf1fea0be7c9ebf2bab6d4eee
Gordon DuQuesnay <gduquesnay@gmail.com>
2018-06-06 09:54:15 +0000

    update task_done script to create the pull request

--- task_done.rb
+++ task_done.rb
@@ -5,20 +5,25 @@ unless task_id
   return
 end
 
+PROJECT_ID= '683768070860536'
+
 require 'active_support/inflector'
 require 'asana'
 
  personal_access_token = ENV.fetch('ASANA_PERSONAL_ACCESS_TOKEN')
- client = Asana::Client.new do |c|
-   c.authentication :access_token, personal_access_token
- end
+ client = Asana::Client.new{ |c| c.authentication :access_token, personal_access_token }
 
  task = client.tasks.find_by_id(task_id)
 
  branch_name = "#{task.name.parameterize}/#{task.id}"
 `git checkout -b #{branch_name}`
 `git add -A`
-`git commit -m "#{task.name} ##{task.id}` 
+
+title = task.name.gsub("`","").strip
+`git commit -m "#{ttle} ##{task.id}"` 
 `git push --set-upstream origin #{branch_name}`
 
+task_url = "https://app.asana.com/0/#{PROJECT_ID}/#{task.id}"
 #pull request
+pr_message = "#{title}\n#{task_url}"
+`hub pull-request -m "#{pr_message}"`
