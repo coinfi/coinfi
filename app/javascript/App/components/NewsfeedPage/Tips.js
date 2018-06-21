c7fcb76e8447da30c5489b4e196c2ec7d36ff0de
Voltron <cryptobotmachine@gmail.com>
2018-06-21 08:47:36 -0400

    implement mobile ui

--- app/javascript/App/components/NewsfeedPage/Tips.js
+++ app/javascript/App/components/NewsfeedPage/Tips.js
@@ -2,43 +2,44 @@ import React from 'react'
 import Icon from '../Icon'
 
 const titleStyle = {
-  paddingBottom:10,
-  borderBottom:'1px solid rgba(0, 0, 0, 0.12)',
-  paddingLeft: 15,
-  fontSize: '1.13rem'
+  fontSize: '1.13rem',
+  fontWeight: 'bold'
 }
+
+const bottomTitleStyle = Object.assign({}, titleStyle)
+
+titleStyle.borderBottom = '1px solid rgba(0, 0, 0, 0.12)',
+titleStyle.paddingLeft = 15
+titleStyle.paddingBottom = 10
+bottomTitleStyle.borderTop = '1px solid rgba(0, 0, 0, 0.12)'
+bottomTitleStyle.paddingTop = 15
+bottomTitleStyle.paddingBottom = 0
+bottomTitleStyle.marginBottom = 0
 
 const listTextNodeStyle = {width:'90%', marginLeft:10}
 
 const listNumberStyle = {
-  float: 'left',
   background: 'rgb(47, 174, 237)',
   borderRadius: 40,
   color: '#fff',
   width: 18,
   paddingLeft: 6,
   paddingTop: 2,
-  fontSize: 10
+  fontSize: 10,
+  verticalAlign: 'top'
 }
 
 export default ({ closeTips }) => {
   return (
     <div className="lh-copy">
-
-
       <div>
-
         <div style={{paddingTop:'15px'}}>
-
           <div style={{float:'right', marginRight: '15px'}} >
             {closeTips && <Icon name="times" className="f4 slate" regular onClick={closeTips} />}
           </div>
-
           <h2 className='tc mt0' style={titleStyle}>CoinFi News</h2>
         </div>
-
         <div style={{margin:20}}>
-
           <div style={{fontSize:'.8rem'}}>
             CoinFi News gives crypto investors like you an informational advantage
             by filtering out the noise and showing you how news is impacting coin
@@ -67,6 +68,29 @@ export default ({ closeTips }) => {
               <div style={listTextNodeStyle}> Enable email and browser notifications to stay up-to-date on the latest market moving news  </div>
             </li>
           </ol>
+
+        </div>
+
+        <div style={{paddingTop:'15px'}}>
+          <h2 className='tc mt0' style={bottomTitleStyle}>Get the most out of CoinFi News</h2>
+          <div style={{margin:20, marginTop:0}}>
+            <div style={{
+              fontSize:'.8rem',
+              textAlign: 'center',
+              marginBottom: 20,
+              marginTop: 10
+            }}>
+              Be the first to know about the latest market moving news
+            </div>
+            <button
+              className="btn btn-blue btn-l w-100"
+              style={{
+                padding: '.8rem 2rem',
+                textTransform: 'none',
+                fontWeight: 'bold'
+              }}
+            >Sign up</button>
+          </div>
         </div>
 
 
