3f5805cd53f3bcccc5d01253b3564d109288f10e
Han Chang <szu.han.chang@gmail.com>
2018-06-24 07:35:53 +0800

    Attempt to get 'Watch' buttons to show up on news coin autosuggest

--- app/javascript/App/components/NewsfeedPage/WatchButton.js
+++ app/javascript/App/components/NewsfeedPage/WatchButton.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import Icon from '../Icon'
 
-const WatchButton = ({ isWatching, coin, updateUser, user, onWatch }) => {
-  if (isWatching(coin.get('id')))
+const WatchButton = ({ isWatching, coin, updateUser, user, onWatch, hasText }) => {
+  const hasTextClassNames = "btn btn-xs btn-gray"
+
+  if (isWatching(coin.get('id'))) {
     return (
       <Icon
         name="star"
         solid
-        className="aqua"
-        onClick={() => updateUser({ unwatchCoin: coin.get('id') })}
-      />
+        className={`aqua ${hasText ? hasTextClassNames : ''}`}
+        onClick={() => updateUser({ unwatchCoin: coin.get('id') })}>
+        {hasText && 'Watching'}
+      </Icon>
     )
+  }
   return (
     <div className="div tooltipped">
       {!user && <div className="tooltip fl">Login to watch</div>}
       <Icon
         name="star"
         light
-        className="light-silver"
+        className={`light-silver ${hasText ? hasTextClassNames : ''}`}
         onClick={() => {
           if (onWatch) onWatch(coin)
           updateUser({ watchCoin: coin.get('id') })
         }}
-      />
+      >
+        {hasText && 'Watch'}
+      </Icon>
     </div>
   )
 }
