f399e9b8c78110dfda72b3e6fa5c178ca47894f9
Han Chang <szu.han.chang@gmail.com>
2018-06-25 11:02:22 +0800

    Refactor FeedSource

--- app/models/feed_source.rb
+++ app/models/feed_source.rb
@@ -1,7 +1,11 @@
 class FeedSource < ApplicationRecord
   SUPERFEEDR_API_URL = 'https://push.superfeedr.com/'.freeze
-  SUPERFEEDR_AUTH = { username: ENV.fetch('SUPERFEEDR_USER'), password: ENV.fetch('SUPERFEEDR_TOKEN') }.freeze
-  SUPERFEEDR_RETRIEVE_BATCH_SIZE = 50.freeze
+  SUPERFEEDR_AUTH = {
+    username: ENV.fetch('SUPERFEEDR_USER'),
+    password: ENV.fetch('SUPERFEEDR_TOKEN'),
+  }.freeze
+  SUPERFEEDR_RETRIEVE_BATCH_SIZE = 50
+  SUPERFEEDR_MAX_PER_PAGE = 500
 
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
@@ -15,75 +19,16 @@ class FeedSource < ApplicationRecord
     pluck(:feed_type).uniq
   end
 
-  def retrieve(before: nil)
-    body = {
-      'hub.mode' => 'retrieve', 
-      'hub.topic' => feed_url, 
-      'format' => 'json', 
-      'count' => SUPERFEEDR_RETRIEVE_BATCH_SIZE
-    }
-
-    body.merge!('before' => before ) if before
-
-    options = {
-      basic_auth: SUPERFEEDR_AUTH,
-      body: body
-    }
-
-    results = HTTParty.get(SUPERFEEDR_API_URL, options)
-
-    items = results['items']
-    puts "Fetched #{items.size} items"
-    last_item_id = items.last.try(:[], "id") #using try in case 0 items
-    puts "Last item fetched: #{last_item_id}"
-    #sleep(1) # to try and avoid API timeouts.  Not sure if we still need this.
-
-    items += retrieve(before: last_item_id) unless items.size < SUPERFEEDR_RETRIEVE_BATCH_SIZE
-    items
-  end
-
-  def retrieve!
-    items = retrieve
-    items.map!{|item| HashWithIndifferentAccess.new(item)} #since ingest is using symbols to access the hash
-    items.each{|item| NewsItemRaw.ingest!(item, slug)}
-  end
-
-  def subscribe!
-    body = {
-      'hub.mode' => 'subscribe', 
-      'hub.topic' => feed_url, 
-      'format' => 'json', 
-      'hub.callback' => callback_url,
-      'hub.secret' => ENV.fetch('SUPERFEEDR_SECRET')
-    }
-
-    options = {
-      basic_auth: { username: ENV.fetch('SUPERFEEDR_USER'), password: ENV.fetch('SUPERFEEDR_TOKEN') },
-      body: body
-    }
-
-    HTTParty.post(SUPERFEEDR_API_URL, options)
-
-    #if the line above fails it throws an error so if we got here
-    #we can assume this one has subscribed sucessfully
-    update(is_subscribed: true)
-  end
-
-  def callback_url
-    "#{self.class.callback_url_base}?source=#{slug}"
-  end
-
   def self.callback_url_base
     protocol = Rails.env.development? ? 'http://' : 'https://'
 
     "#{protocol}#{ENV.fetch('ROOT_DOMAIN')}/webhooks/#{ENV.fetch('SUPERFEEDR_CALLBACK_URL_SEGMENT_SECRET')}-superfeedr-ingest"
   end
 
-  SUPERFEEDR_MAX_PER_PAGE = 500
   def self.fetch_subs(page = 1)
-    #TODO change this now that we have > 500 subscriptions
+    # TODO: Change this now that we have > 500 subscriptions
     body = {
-      'hub.mode' => 'list', 
+      'hub.mode' => 'list',
       'by_page' => SUPERFEEDR_MAX_PER_PAGE, # This is the max supported by SuperFeedr
       'page' => page
     }
@@ -121,21 +66,76 @@ class FeedSource < ApplicationRecord
     fs_ids_missing_subs
   end
 
-  def subscribed?
-    query = {
-      'hub.mode' => 'list', 
-      'search[feed][url]' => feed_url, 
-      'format' => 'json', 
+  def retrieve(before: nil)
+    body = {
+      'hub.mode' => 'retrieve',
+      'hub.topic' => feed_url,
+      'format' => 'json',
+      'count' => SUPERFEEDR_RETRIEVE_BATCH_SIZE
+    }
+
+    body.merge!('before' => before ) if before
+
+    options = {
+      basic_auth: SUPERFEEDR_AUTH,
+      body: body
+    }
+
+    results = HTTParty.get(SUPERFEEDR_API_URL, options)
+
+    items = results['items']
+    puts "Fetched #{items.size} items"
+    last_item_id = items.last.try(:[], "id") # using try in case 0 items
+    puts "Last item fetched: #{last_item_id}"
+
+    items += retrieve(before: last_item_id) unless items.size < SUPERFEEDR_RETRIEVE_BATCH_SIZE
+    items
+  end
+
+  def retrieve!
+    items = retrieve
+    items.map!{|item| HashWithIndifferentAccess.new(item)} # since ingest is using symbols to access the hash
+    items.each{|item| NewsItemRaw.ingest!(item, slug)}
+  end
+
+  def subscribe!
+    body = {
+      'hub.mode' => 'subscribe',
+      'hub.topic' => feed_url,
+      'format' => 'json',
+      'hub.callback' => callback_url,
+      'hub.secret' => ENV.fetch('SUPERFEEDR_SECRET')
     }
 
     options = {
-      basic_auth: { username: ENV.fetch('SUPERFEEDR_USER'), password: ENV.fetch('SUPERFEEDR_TOKEN') },
+      basic_auth: SUPERFEEDR_AUTH,
+      body: body
+    }
+
+    HTTParty.post(SUPERFEEDR_API_URL, options)
+
+    # If the line above fails it throws an error so if we got here,
+    # we can assume this one has subscribed successfully.
+    update(is_subscribed: true)
+  end
+
+  def subscribed?
+    query = {
+      'hub.mode' => 'list',
+      'search[feed][url]' => feed_url,
+      'format' => 'json',
+    }
+
+    options = {
+      basic_auth: SUPERFEEDR_AUTH,
       query: query
     }
 
-    #debug_url = "http://tester123.proxy.beeceptor.com"
     subs_with_this_feed_url = HTTParty.get(SUPERFEEDR_API_URL, options)
-    #subs_with_this_feed_url = HTTParty.get(debug_url, options)
     subs_with_this_feed_url.any?{|sub| sub["subscription"]["endpoint"] == callback_url}
+  end
+
+  def callback_url
+    "#{self.class.callback_url_base}?source=#{slug}"
   end
 end
