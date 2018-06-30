2be654aefc25eb799f0ccf0f7dae937550528771
Han Chang <szu.han.chang@gmail.com>
2018-06-30 20:39:13 +0800

    Update team members

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,88 +1,81 @@
 class TeamMember < ActiveHash::Base
-  self.data = [ 
-    { 
+  self.data = [
+    {
       name: 'Timothy Tam',
       title: 'Co-founder',
       bio: '10+ years in equities trading and hedge funds. Started his career in Goldman Sachs Asia, before working as a senior trader in two Asia hedge funds each with $1+ billion assets under management. In his last role he was cofounder of an Asia Long Short Hedge Fund that specialized in event trading.',
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671951/profile-photos/tim-tam.jpg',
       linkedin: 'https://www.linkedin.com/in/ttam-0190212/'
     },
-    { 
+    {
       name: 'Han Chang',
       title: 'Co-founder',
       bio: "Master's in Computer Science from Boston University. Early Bitcoin investor, founded a Chinese Bitcoin exchange; previously at MIT Lincoln Labs, OkCupidLabs, General Assembly, and Lime Brokerage. 8+ years of professional experience developing web applications serving 10M+ users as well as writing black box equity trading algorithms.",
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671945/profile-photos/han-chang.jpg',
       linkedin: 'https://www.linkedin.com/in/hanchang0/'
     },
-    { 
+    {
       name: 'Nate Tsang',
       title: 'Co-founder',
       bio: "Holds a Juris Doctor from UAlberta Law. 7+ years of experience as a growth hacker in media startups as well as developing multiple web properties that have cumulatively grown from zero to 10M+ pageviews. Co-founder of a keyword intelligence platform for digital marketers, acquired in 2016.",
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671949/profile-photos/nate-tsang.jpg',
       linkedin: 'https://www.linkedin.com/in/nate-tsang-3a493a153/'
     },
-    { 
-      name: 'Duc Tran',
-      title: 'Research Analyst',
-      bio: "Master's from Princeton University. Experienced research analyst who has deep consulting experience in the financial services industry investigating the impact of fiscal and government policy on market movements. Experienced with statistical methods using STATA, R, and Excel.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671944/profile-photos/duc-tran.jpg',
-      linkedin: 'https://www.linkedin.com/in/duc-tran-05b4242a/'
+    {
+      name: 'Jonney Liu',
+      title: 'Head of Crypto Research',
+      bio: "Formerly a senior portfolio manager with 11+ years experience trading derivatives at institutions such as Lehman Brothers, Macquarie; most recently, he was at a $350 million volatility arbitrage hedge fund from True Capital.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1530362110/profile-photos/Jonney_yxi8pk.jpg',
+      linkedin: 'https://www.linkedin.com/in/jonney-liu-0453824/'
     },
-    { 
-      name: 'Nav Aulakh',
-      title: 'Community Manager',
-      bio: "Programmatic digital media planner who moved client side, helping tech SaaS startups with community management and marketing. Experienced crypto investor with a background in Mathematical Risk Management (BSc) and Financial Engineering (MSc).",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671950/profile-photos/navneet-aulakh.jpg',
-      linkedin: 'https://www.linkedin.com/in/navneet-aulakh-73161826'
+    {
+      name: 'Alexander Svanevik',
+      title: 'Chief Data Scientist',
+      bio: "Manager at Schibsted Media Group, 8+ years of experience leading a group of 7 data scientists to create business value from data. Previously a management consultant at BearingPoint as well as cofounder of Codeus Artificial Intelligence. Experienced crypto trader who runs ICO Whitelists.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671943/profile-photos/alexander-svanevik.jpg',
+      linkedin: 'https://www.linkedin.com/in/alexander-svanevik-152198a/'
     },
-    { 
-      name: 'Sam Peacey',
-      title: 'Full-stack Web Engineer',
-      bio: "Experienced fullstack web engineer, multi-talented with Ruby on Rails and JavaScript including jQuery, Promises, ES6, await/async, React, Babel; has set up continuous integration and automated testing environments as well as scaled multiple production web applications.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671949/profile-photos/sam-peacey.jpg',
-      linkedin: 'https://www.linkedin.com/in/sam-peacey-2b016b121/'
+    {
+      name: 'Toni Cebrian',
+      title: 'Lead Data Engineer',
+      bio: "Former head of Data Science at Stuart, holds a Master of Science in Artificial Intelligence from the Polytechnic University of Catalonia. Professionally developed software for 18+ years specifically focusing on data projects.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1530362109/profile-photos/Toni_kty0aw.png',
+      linkedin: 'https://www.linkedin.com/in/tonicebrian/'
     },
-    { 
-      name: 'Jake Broughton',
-      title: 'Full-stack Web Engineer',
-      bio: "Solution-finder and full-stack software engineer, currently specializing in Rails, React, and Redux architecture. Growing involvement in cryptocurrency tech & trade, and proponent of the ICO paradigm.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1525438291/profile-photos/jake-broughton.jpg',
-      linkedin: 'https://www.linkedin.com/in/jakebroughton/'
-    },
-    { 
-      name: 'Alex Lascus',
-      title: 'Community Support',
-      bio: "Internet marketing expert with over 5 years experience in Advertising and Community Management. Passionate about anything that’s related to social media and cryptocurrencies.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/alex-lascus.png',
-      linkedin: ''
-    },
-    { 
-      name: 'Elliot Sommers',
-      title: 'Community Support',
-      bio: "Senior majoring in Management Information Systems at the University of Northwestern - St. Paul. Background in corporate IT support and technical consulting. Early blockchain investor and active day trader.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671947/profile-photos/elliot-sommers.jpg',
-      linkedin: ''
-    },
-    { 
-      name: 'Tanushree Jain',
-      title: 'Community Support',
-      bio: "Experienced Customer Relationship Manager and Researcher with an MBA in Entrepreneurship from Bentley University, MA. Passionate about new technology, transitioned from a background in Biotechnology (MSc) to cryptocurrency.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671949/profile-photos/tanushree-jain.jpg',
-      linkedin: ''
-    },
-    { 
+    {
       name: 'Zachary Pardes',
       title: 'PR Advisor',
       bio: "Integrated communications professional with 10 years of experience leading earned media campaigns for Virgin, E*TRADE, B2R Finance, Citi, and Investing.com. Managed $1M+ digital strategies for NYC / Silicon Valley startups. Master's from Northwestern University.",
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522674036/profile-photos/zachary-pardes.jpg',
       linkedin: 'https://www.linkedin.com/in/zpardes/'
     },
-    { 
-      name: 'Alexander Svanevik',
-      title: 'Chief Data Scientist',
-      bio: "Data Science Manager at Schibsted Media Group, 8+ years of experience currently leading a group of 7 data scientists to create business value from data. Previously a management consultant at BearingPoint as well as cofounder of Codeus Artificial Intelligence. Experienced crypto trader who runs ICO Whitelists.",
-      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671943/profile-photos/alexander-svanevik.jpg',
-      linkedin: 'https://www.linkedin.com/in/alexander-svanevik-152198a/'
+    {
+      name: 'Nav Aulakh',
+      title: 'Community Manager',
+      bio: "Programmatic digital media planner who moved client side, helping tech SaaS startups with community management and marketing. Experienced crypto investor with a background in Mathematical Risk Management (BSc) and Financial Engineering (MSc).",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671950/profile-photos/navneet-aulakh.jpg',
+      linkedin: 'https://www.linkedin.com/in/navneet-aulakh-73161826'
+    },
+    {
+      name: 'Alex Lascus',
+      title: 'Community Support',
+      bio: "Internet marketing expert with over 5 years experience in Advertising and Community Management. Passionate about anything that’s related to social media and cryptocurrencies.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/alex-lascus.png',
+      linkedin: ''
+    },
+    {
+      name: 'Elliot Sommers',
+      title: 'Community Support',
+      bio: "Senior majoring in Management Information Systems at the University of Northwestern - St. Paul. Background in corporate IT support and technical consulting. Early blockchain investor and active day trader.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671947/profile-photos/elliot-sommers.jpg',
+      linkedin: ''
+    },
+    {
+      name: 'Tanushree Jain',
+      title: 'Community Support',
+      bio: "Experienced Customer Relationship Manager and Researcher with an MBA in Entrepreneurship from Bentley University, MA. Passionate about new technology, transitioned from a background in Biotechnology (MSc) to cryptocurrency.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671949/profile-photos/tanushree-jain.jpg',
+      linkedin: ''
     },
   ]
 end
