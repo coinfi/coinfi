d80ea3b9ace6f12889a873f0d5a718c935cd25a5
Voltron <cryptobotmachine@gmail.com>
2018-07-02 06:40:23 -0400

    show label in filter tags for date filters

--- app/javascript/App/components/FilterTags.js
+++ app/javascript/App/components/FilterTags.js
@@ -3,19 +3,27 @@ import { FilterButtonValue } from './Filters/FilterButton'
 
 const FilterTags = (props) => {
   const { removeFilter, activeFilters } = props
+  let tagLabel = ''
   return (
     <div>
-      {activeFilters.map((filter) => (
+      {activeFilters.map((filter) => {
+        if (filter.get('key') === 'publishedSince') tagLabel = 'from'
+        if (filter.get('key') === 'publishedUntil') tagLabel = 'to'
+        if (filter.get('key') !== 'publishedUntil' && filter.get('key') !== 'publishedSince') tagLabel = ''
+        return (
         <div
           key={filter.get('key')}
           className="coin-filter-tag pa1"
         >
-          <FilterButtonValue filter={filter} />
+          {tagLabel} <FilterButtonValue filter={filter} />
           <a className="ml2 mr1 gray" onClick={() => removeFilter(filter.get('key'))}>
             <i className="icon far fa-times" />
           </a>
         </div>
-      ))}
+        )
+      }
+
+      )}
     </div>
   )
 }
