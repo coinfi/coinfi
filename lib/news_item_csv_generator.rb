3505d2ee06168168913ec206a0d9ee75359d99ec
Han Chang <szu.han.chang@gmail.com>
2018-07-02 11:30:43 +0800

    Eager load associations for NewsItemCsvGenerator for performance

--- lib/news_item_csv_generator.rb
+++ lib/news_item_csv_generator.rb
@@ -2,7 +2,7 @@ class NewsItemCsvGenerator
   def self.to_csv(news_items, options = {})
     column_headers = %w[id, coin_symbols, news_categories, user_email, title, created_at]
     CSV.generate(options) do |csv|
-      news_items.each do |news_item|
+      news_items.includes(:news_coin_mentions, :coins, :news_item_categorizations, :news_categories).each do |news_item|
         csv << [
           news_item.id,
           news_item.coin_symbols,
