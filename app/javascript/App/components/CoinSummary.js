51b7ad3b24efd32bb97f120ff7972822ddeb440f
Voltron <cryptobotmachine@gmail.com>
2018-07-03 00:55:24 -0400

    define test components

--- app/javascript/App/components/CoinSummary.js
+++ app/javascript/App/components/CoinSummary.js
@@ -1,5 +1,18 @@
-export default MyComponent => {
-  return <div className={ props.className }>Hello { props.name }!</div>
-}
+import React from "react"
+import styled from 'styled-components'
 
+const Button = styled.button`
+background: palevioletred;
+border-radius: 3px;
+border: none;
+color: white;
+`;
+
+const TomatoButton = styled(Button)`
+background: tomato;
+`;
+
+export default props => {
+  return <Button text={'test'}>foo</Button>
+}
 
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import CoinSummary from './CoinSummary'
-import CoinDetail from './CoinDetail'
+import CoinSummary from '../app/javascript/App/components/CoinSummary'
+import CoinDetail from '../app/javascript/App/components/CoinDetail'
 
 
 storiesOf('Coin', module).add('summary', () => <CoinSummary  />);
