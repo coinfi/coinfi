8a6105977fbd7a2eb13bcf365788fd25664379ca
Voltron <cryptobotmachine@gmail.com>
2018-07-03 00:26:33 -0400

    rm unused code

--- app/javascript/App/components/FilterPanel/filterComponents/Dates.js
+++ app/javascript/App/components/FilterPanel/filterComponents/Dates.js
@@ -1,15 +1,13 @@
-import React, { Fragment } from 'react'
-import Type from 'prop-types'
-import ItemSelectorDate from '../../ItemSelectorDate'
+import React, { Fragment } from "react";
+import Type from "prop-types";
+import ItemSelectorDate from "../../ItemSelectorDate";
 
 const Dates = ({ dates, value, onChange }) => (
-  <Fragment>
-    <ItemSelectorDate
-      items={dates}
-      selectedItems={value}
-      onChange={onChange('dates')}
-    />
-  </Fragment>
-)
+  <ItemSelectorDate
+    items={dates}
+    selectedItems={value}
+    onChange={onChange("dates")}
+  />
+);
 
-export default Dates
+export default Dates;
