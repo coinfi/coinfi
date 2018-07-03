b1310566058fbe995c1033c3cd58d6dc3bc457bf
Voltron <cryptobotmachine@gmail.com>
2018-07-03 01:09:30 -0400

    :construction: explore ways to componentize existing react components

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,9 +4,13 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import CoinSummary from '../app/javascript/App/components/CoinSummary'
 import CoinDetail from '../app/javascript/App/components/CoinDetail'
+import { BodySection } from '../app/javascript/App/components/NewsfeedPage/BodySection'
+
+import { bodySectionProps } from './bodySectionProps'
 
 
 storiesOf('Coin', module).add('summary', () => <CoinSummary  />);
 
 storiesOf('Coin', module).add('detail', () => <CoinDetail  />);
 
+storiesOf('Coin', module).add('body section', () => <BodySection bucket={bodySectionProps}  />);
