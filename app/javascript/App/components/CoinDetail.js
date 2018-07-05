9577f00ddc16d3722fbac2cc1dd710ebf76b1466
Voltron <35572666+cryptoB0T@users.noreply.github.com>
2018-07-05 09:08:14 -0400

    Feature/storybook poc/#730629677501460 (#195)
    * wip
    
    * stub two stories
    
    * define test components
    
    * :construction: explore ways to componentize existing react components
    
    * stub coin summary and detail stories
    
    * add style

---
+++ app/javascript/App/components/CoinDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default (props) => {
+  return (
+    <div>
+      <div>Fundamentals</div>
+      <div>Price Chart</div>
+      <div>Links</div>
+    </div>
+  )
+}
+
+
---
+++ stories/index.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import CoinSummary from '../app/javascript/App/components/CoinSummary'
+import CoinDetail from '../app/javascript/App/components/CoinDetail'
+
+import { BodySection } from '../app/javascript/App/components/NewsfeedPage/BodySection'
+import { NewsBody } from '../app/javascript/App/components/NewsfeedPage/NewsBody'
+import { NewsCoinTags } from '../app/javascript/App/components/NewsfeedPage/NewsCoinTags'
+
+import { bodySectionProps } from './bodySectionProps'
+
+import styles from './tachy.css'
+
+
+storiesOf('Coin', module).add('summary', () => <CoinSummary  />);
+storiesOf('Coin', module).add('detail', () => <CoinDetail  />);
+
+/*
+storiesOf('News', module).add('body section', () => <BodySection bucket={bodySectionProps}  />);
+storiesOf('News', module).add('news body', () => <NewsBody {bodySectionProps.bodyProps}  />);
+storiesOf('News', module).add('news coin tags', () => <NewsBody {bodySectionProps.bodyProps}  />);
+*/
