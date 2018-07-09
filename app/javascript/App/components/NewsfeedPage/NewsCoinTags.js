646abc712951a236751be914743681d59a5dbf7f
Voltron <cryptobotmachine@gmail.com>
2018-07-09 10:13:27 +0800

    :fire: clean formatting

--- app/javascript/App/components/NewsfeedPage/NewsCoinTags.js
+++ app/javascript/App/components/NewsfeedPage/NewsCoinTags.js
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const NewsCoinTags = ({ newsItem, selectCoin }) => (
-    <div>
-      {newsItem.get('coin_link_data').map((data, index) => (
-          <a
-            key={index}
-            className="tag pointer"
-            onClick={() => selectCoin(data)}
-          >
-            {data.get('symbol')}
-          </a>
-        )
-      )}
-    </div>
-  )
+  <div>
+    {newsItem.get("coin_link_data").map((data, index) => (
+      <a key={index} className="tag pointer" onClick={() => selectCoin(data)}>
+        {data.get("symbol")}
+      </a>
+    ))}
+  </div>
+)
 
 export default NewsCoinTags
