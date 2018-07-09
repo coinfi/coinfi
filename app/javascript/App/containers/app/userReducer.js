691b90287e81bc23907fb5bbd4e83bf4245698e4
Han <han@coinfi.com>
2018-07-09 18:17:34 +0800

    Filter Newsfeed by Watchlist #733324407774222 (#210)

--- app/javascript/App/containers/app/userReducer.js
+++ app/javascript/App/containers/app/userReducer.js
@@ -6,6 +6,7 @@ export default (state = initialState, action) => {
   const { type, response } = action
   switch (type) {
     case 'SET_USER':
+          debugger
       if (response) return fromJS(response)
       return state
     default:
