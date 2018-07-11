979d54a6bf52469f32f0c46c4d995c0c451dad3d
Han Chang <szu.han.chang@gmail.com>
2018-07-11 16:31:54 +0800

    Remove debugger statement

--- app/javascript/App/containers/app/userReducer.js
+++ app/javascript/App/containers/app/userReducer.js
@@ -6,7 +6,6 @@ export default (state = initialState, action) => {
   const { type, response } = action
   switch (type) {
     case 'SET_USER':
-          debugger
       if (response) return fromJS(response)
       return state
     default:
