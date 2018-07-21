82ffa9714109f0e93bbb11d2b7606555193901db
Voltron <cryptobotmachine@gmail.com>
2018-07-21 12:03:58 -0400

    implement styled component examples

--- app/javascript/App/components/CoinShow/index.js
+++ app/javascript/App/components/CoinShow/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { Layout, Card, Button, Tabs, Menu, Dropdown, Icon } from 'antd'
-import {} from 'antd'
+import styled from 'styled-components'
 
 const TabPane = Tabs.TabPane
 const { Header, Footer, Content } = Layout
@@ -10,6 +10,26 @@ const menu = (
     <Menu.Item key="2">BTC</Menu.Item>
   </Menu>
 )
+const FlexGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+const FlexGridItem = styled.div`
+  height: 80px;
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 33.3333%;
+  flex-basis: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 600px) {
+    flex-basis: 450px;
+  }
+  @media (min-width: 900px) {
+    flex-basis: 300px;
+  }
+`
 
 export default class CoinShow extends Component {
   componentDidMount() {
@@ -21,30 +41,33 @@ export default class CoinShow extends Component {
       <Fragment>
         <Layout>
           <Content>
-            <div>
-              <section>
-                <Dropdown overlay={menu}>
-                  <Button style={{ marginLeft: 8 }}>
-                    USD <Icon type="down" />
-                  </Button>
-                </Dropdown>
-                <Button>Watch coin</Button>
-              </section>
-              {/* <Dropdown><Button>USD</Button></Dropdown> */}
-              <img
-                alt="Bitcoin"
-                height="32"
-                src="https://gitcdn.link/repo/cjdowner/cryptocurrency-icons/master/svg/color/btc.svg"
-                width="32"
-              />
-              <section>
-                <span>Bitcoin</span> <span>BTC</span>
-              </section>
-              <section>
-                <span>$7,370.86</span>
-                <span>-1.34%</span>
-              </section>
-            </div>
+            <section>
+              <Dropdown overlay={menu}>
+                <Button style={{ marginLeft: 8 }}>
+                  USD <Icon type="down" />
+                </Button>
+              </Dropdown>
+              <Button>Watch coin</Button>
+            </section>
+            <section>
+              <FlexGrid>
+                <FlexGridItem>
+                  <img
+                    alt="Bitcoin"
+                    height="32"
+                    src="https://gitcdn.link/repo/cjdowner/cryptocurrency-icons/master/svg/color/btc.svg"
+                    width="32"
+                  />
+                </FlexGridItem>
+                <FlexGridItem>
+                  <span>Bitcoin</span> <span>BTC</span>
+                </FlexGridItem>
+                <FlexGridItem>
+                  <span>$7,370.86</span>
+                  <span>-1.34%</span>
+                </FlexGridItem>
+              </FlexGrid>
+            </section>
 
             <Tabs defaultActiveKey="1">
               <TabPane tab="Overview" key="1">
