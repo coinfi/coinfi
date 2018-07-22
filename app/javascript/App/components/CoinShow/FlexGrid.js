65a170ba4863b63bfae8fe4867fcc0a5b92985d4
Voltron <cryptobotmachine@gmail.com>
2018-07-21 21:16:59 -0400

    components accept width value to handle setting some panels wider

---
+++ app/javascript/App/components/CoinShow/FlexGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FlexGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export default FlexGrid
---
+++ app/javascript/App/components/CoinShow/FlexGridItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+class FlexGridItemElem extends React.Component {
+  render() {
+    const flexBasisWidth = 300
+    const colGridWidth = this.props.colWidth * flexBasisWidth || flexBasisWidth
+    const FlexGridItem = styled.div`
+      height: 80px;
+      flex-grow: 1;
+      flex-shrink: 0;
+      flex-basis: 33.3333%;
+      flex-basis: ${flexBasisWidth}px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      @media (min-width: 600px) {
+        flex-basis: ${flexBasisWidth}px;
+      }
+      @media (min-width: 900px) {
+        flex-basis: ${colGridWidth}px;
+      }
+    `
+    return <FlexGridItem>{this.props.children}</FlexGridItem>
+  }
+}
+
+export default FlexGridItemElem
