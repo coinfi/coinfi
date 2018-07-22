085843b2af2cc6d95cc48f7c3a3caf4379bae25b
Voltron <cryptobotmachine@gmail.com>
2018-07-21 22:23:21 -0400

    use styled component for tag styles

--- app/javascript/App/components/CoinShow/index.js
+++ app/javascript/App/components/CoinShow/index.js
@@ -6,16 +6,6 @@ import FlexGridItem from './FlexGridItem'
 
 const TabPane = Tabs.TabPane
 const { Header, Footer, Content } = Layout
-const menu = (
-  <Menu>
-    <Menu.Item key="1">USD</Menu.Item>
-    <Menu.Item key="2">BTC</Menu.Item>
-  </Menu>
-)
-
-const cardStyle = {
-  flexGrow: 1,
-}
 
 export default class CoinShow extends Component {
   componentDidMount() {
@@ -36,25 +26,23 @@ export default class CoinShow extends Component {
               </Dropdown>
               <Button>Watch coin</Button>
             </ButtonWrap>
-            <section>
-              <FlexGrid>
-                <FlexGridItem>
-                  <img
-                    alt="Bitcoin"
-                    height="32"
-                    src="https://gitcdn.link/repo/cjdowner/cryptocurrency-icons/master/svg/color/btc.svg"
-                    width="32"
-                  />
-                </FlexGridItem>
-                <FlexGridItem>
-                  <span>Bitcoin</span> <span>BTC</span>
-                </FlexGridItem>
-                <FlexGridItem>
-                  <span>$7,370.86</span>
-                  <span>-1.34%</span>
-                </FlexGridItem>
-              </FlexGrid>
-            </section>
+            <Section>
+              <Div>
+                <img
+                  alt="Bitcoin"
+                  height="32"
+                  src="https://gitcdn.link/repo/cjdowner/cryptocurrency-icons/master/svg/color/btc.svg"
+                  width="32"
+                />
+              </Div>
+              <Div>
+                <span>Bitcoin</span> <span>BTC</span>
+              </Div>
+              <Div>
+                <span>$7,370.86</span>
+                <span>-1.34%</span>
+              </Div>
+            </Section>
 
             <Tabs defaultActiveKey="1">
               <TabPane tab="Overview" key="1">
@@ -95,3 +83,27 @@ const ButtonWrap = styled.div`
   text-align: right;
   margin: 1rem;
 `
+
+const Section = styled.section`
+  text-align: center;
+  @media (min-width: 900px) {
+    text-align: left;
+  }
+`
+
+const Div = styled.div`
+  @media (min-width: 900px) {
+    display: inline-block;
+  }
+`
+
+const menu = (
+  <Menu>
+    <Menu.Item key="1">USD</Menu.Item>
+    <Menu.Item key="2">BTC</Menu.Item>
+  </Menu>
+)
+
+const cardStyle = {
+  flexGrow: 1,
+}
