870add84bf3ba20315abd63d3a55ed05457b9943
Voltron <cryptobotmachine@gmail.com>
2018-07-24 10:46:05 -0400

    show coin ranking in table #755023945515449

--- app/javascript/App/components/CoinIndex/index.js
+++ app/javascript/App/components/CoinIndex/index.js
@@ -17,72 +17,33 @@ const { Header, Footer, Content } = Layout
 
 class CoinIndex extends Component {
   state = {
-    filteredInfo: null,
     sortedInfo: null,
   }
+
   handleChange = (pagination, filters, sorter) => {
     this.setState({
-      filteredInfo: filters,
       sortedInfo: sorter,
     })
   }
 
-  clearFilters = () => {
-    this.setState({ filteredInfo: null })
-  }
-
   clearAll = () => {
     this.setState({
-      filteredInfo: null,
       sortedInfo: null,
     })
   }
 
-  setAgeSort = () => {
-    this.setState({
-      sortedInfo: {
-        order: 'descend',
-        columnKey: 'age',
-      },
-    })
-  }
   render() {
-    let { sortedInfo, filteredInfo } = this.state
+    let { sortedInfo } = this.state
     sortedInfo = sortedInfo || {}
-    filteredInfo = filteredInfo || {}
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-        sorter: (a, b) => a.age - b.age,
-        sortOrder: sortedInfo.columnKey === 'age' && sortedInfo.order,
-      },
-      {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
-      },
-    ]
 
     const { symbol } = this.props
 
     const columnNames = [
+      {
+        title: '#',
+        dataIndex: 'ranking',
+        key: 'ranking',
+      },
       {
         title: 'Name',
         dataIndex: 'name',
