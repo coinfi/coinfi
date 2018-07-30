14cc1a958effc1b312496d296ed0ac8776439b89
Voltron <cryptobotmachine@gmail.com>
2018-07-30 07:47:46 -0400

    add total_count to api response #760453548221107

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -1,10 +1,13 @@
 class Api::CoinsController < ApiController
+
+  include Rails::Pagination
+
   def index
     query = params[:q] || {}
     if params[:exclude_watched]
       query[:id_not_in] = current_user.watchlist.coin_ids
     end
-    @coins = Coin.order(:ranking).ransack(query).result(distinct: true).page(params[:page]).limit(params[:limit] || 10)
+    @coins = paginate Coin.order(:ranking).ransack(query).result(distinct: true).limit(params[:limit] || 10)
     respond_success index_serializer(@coins)
   end
 
