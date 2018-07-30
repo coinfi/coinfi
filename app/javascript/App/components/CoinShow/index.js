db91915d43178e8021f3998ff9bcac56e86a9f03
Voltron <cryptobotmachine@gmail.com>
2018-07-29 20:00:43 -0400

    show red and green for percentage values in fundamentals panel #759207414376201

--- app/javascript/App/components/CoinShow/index.js
+++ app/javascript/App/components/CoinShow/index.js
@@ -52,11 +52,31 @@ class CoinShow extends Component {
       },
       {
         title: '24HR',
-        value: `${coinObj.change24h.toLocaleString('en-US')}%`,
+        value: (
+          <span
+            style={
+              coinObj.change24h > 0
+                ? { color: '#12d8b8' }
+                : { color: '#ff6161' }
+            }
+          >
+            {coinObj.change24h.toLocaleString('en-US')}%
+          </span>
+        ),
       },
       {
         title: '7D',
-        value: `${coinObj.change7d.toLocaleString('en-US')}%`,
+        value: (
+          <span
+            style={
+              coinObj.change7d > 0
+                ? { color: '#12d8b8' }
+                : { color: '#ff6161' }
+            }
+          >
+            {coinObj.change7d.toLocaleString('en-US')}%
+          </span>
+        ),
       },
       {
         title: 'Circulating supply',
