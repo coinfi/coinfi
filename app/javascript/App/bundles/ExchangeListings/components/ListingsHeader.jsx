dffedf79bfe4fcbc7ee7a8c3c57dca4ce0157861
Han Chang <szu.han.chang@gmail.com>
2018-08-07 10:28:36 +0800

    Outline around ExchangeListings header

--- app/javascript/App/bundles/ExchangeListings/components/ListingsHeader.jsx
+++ app/javascript/App/bundles/ExchangeListings/components/ListingsHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ListingsHeader = (props) => {
   return (
-    <div className="flex flex-auto f6 bg-athens">
+    <div className="flex flex-auto f6 bg-athens b--b">
       <div className="fl w-third pa2">Pair</div>
       <div className="fl w-third pa2">Exchange</div>
       <div className="fl w-third pa2">Date Detected</div>
