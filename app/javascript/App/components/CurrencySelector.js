240c6f374de657c458387ac990ff87d01069af52
Voltron <cryptobotmachine@gmail.com>
2018-08-10 13:18:03 -0400

    tweak currency dropdown coin index #775432670358049

--- app/javascript/App/components/CurrencySelector.js
+++ app/javascript/App/components/CurrencySelector.js
@@ -11,9 +11,16 @@ const currencyMenu = (changeCurrencyHandler) => (
 const CurrencySelector = ({ currency, changeCurrencyHandler }) => (
   <Dropdown overlay={currencyMenu(changeCurrencyHandler)}>
     <Button>
-      {currency === 'USD' && 'USD'}
-      {currency === 'BTC' && 'BTC'}
-      <i className="material-icons">expand_more</i>
+      <span style={{ position: 'relative', top: -5, left: -8 }}>
+        {currency === 'USD' && 'USD'}
+        {currency === 'BTC' && 'BTC'}
+      </span>
+      <i
+        className="material-icons"
+        style={{ top: 2, marginLeft: -15, position: 'relative', left: 12 }}
+      >
+        expand_more
+      </i>
     </Button>
   </Dropdown>
 )
