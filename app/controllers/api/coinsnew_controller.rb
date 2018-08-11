aa4da2c8aeeb05729489ebb9b3c201e333351244
Han Chang <szu.han.chang@gmail.com>
2018-08-11 16:49:26 +0800

    Fix Coins table ranking column width, coin icons #775442626877193

--- app/controllers/api/coinsnew_controller.rb
+++ app/controllers/api/coinsnew_controller.rb
@@ -1,7 +1,7 @@
 class Api::CoinsnewController < ApiController
   def index
     @current_page = params[:page] || 1
-    @coins = Coin.select(:id, :ranking, :name, :symbol, :price, :market_cap, :change1h, :change24h, :change7d, :volume24)
+    @coins = Coin.select(:id, :ranking, :image_url, :name, :symbol, :price, :market_cap, :change1h, :change24h, :change7d, :volume24)
       .page(@current_page).per(params[:per]).order(:ranking)
 
     render json: @coins, methods: :sparkline
