723a69887ade2e8ee61e4269d2075bade9ac3510
Voltron <cryptobotmachine@gmail.com>
2018-08-13 12:15:49 -0400

    build functionality to enable dark mode stylesheet

---
+++ app/javascript/modules/navigation/mobile-nav.js
@@ -0,0 +1,61 @@
+
+export const toggleOverlay = (element) => {
+  $('body').toggleClass('overflow-hidden')
+  const e = $(element)
+  let c = ['active', 'visible']
+  let delay = 1
+  if (e.hasClass('active')) {
+    c.reverse()
+    delay = 750
+  }
+  e.toggleClass(c[0])
+  setTimeout(() => {
+    e.toggleClass(c[1])
+  }, delay)
+  return e
+}
+
+$(document).ready(() => {
+  $('.mobile-nav-toggle').on('click', () => toggleOverlay($('#mobile-nav')))
+
+  $('.search-toggle').on('click', () => {
+    toggleOverlay($('#global-coin-search'))
+      .find('input[type="text"]')
+      .first()
+      .focus()
+  })
+
+  $('.registration-form-wrapper form').on('submit', function() {
+    $(this)
+      .find('input[type=submit]')
+      .prop('disabled', true)
+      .text('Processing...')
+  })
+
+  if (document.querySelector('.register-set-password') !== null)
+    fbq('track', 'Lead', {})
+})
+
+export const darkModeFeature = () => {
+  const darkModeBtn = document.querySelector('.trigger-dark-mode')
+  darkModeBtn.addEventListener('click', function(event) {
+    window.darkModeEnabled = !window.darkModeEnabled
+    console.log('add syle')
+    var head = document.head
+    var link = document.createElement('link')
+
+    link.type = 'text/css'
+    link.rel = 'stylesheet'
+    link.className = 'mobile-nav-css'
+    link.href = '/assets/dark-mode.css'
+
+    if (darkModeEnabled) {
+      head.appendChild(link)
+    } else {
+      const darkStylesheet = document.querySelector('.mobile-nav-css')
+      document.getElementsByTagName('head')[0].removeChild(darkStylesheet)
+    }
+  })
+}
+
+darkModeFeature()
