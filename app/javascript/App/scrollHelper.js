b24a499e9f8750d9127605a47d612969d733c008
Han Chang <szu.han.chang@gmail.com>
2018-08-15 11:52:15 +0800

    Tweak height to show properly, add comment doc #778556282442853

--- app/javascript/App/scrollHelper.js
+++ app/javascript/App/scrollHelper.js
@@ -1,12 +1,16 @@
 export default (element) => {
-  // fixes issue in firefox where users are unable to scroll
+  // Workarounds for Firefox overflow bug - for more information, see:
+  // https://app.nuclino.com/CoinFi/Engineering/Handle-Firefox-Scrolling-Issues-724ec6e6-3d06-453b-b8fb-62582f796833
   const newsfeedElem = document.querySelector('#newsfeed')
   const newsBodyElem = document.querySelector('.selected-news-content')
   const coinDrawerElem = document.querySelector('.coin-watch-list')
   const bodyTag = document.querySelector('body').offsetHeight
   const topNav = document.querySelector('.topnav').offsetHeight
-  const coinSearchHeight = document.querySelector('.search-coin-wrapper').offsetHeight
-  const calculatedNewsListHeight = bodyTag - topNav - coinSearchHeight
+  const coinSearchHeight = document.querySelector('.search-coin-wrapper')
+    .offsetHeight
+  // TODO: Figure out where this 30px is coming from...
+  const calculatedNewsListHeight = bodyTag - topNav - coinSearchHeight - 30
+  console.log(calculatedNewsListHeight)
 
   if (!!newsfeedElem) {
     newsfeedElem.style.maxHeight = `${calculatedNewsListHeight}px`
