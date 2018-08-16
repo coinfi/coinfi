55f28ea58ef05835a90fc6c3a8b9df5114e5bd97
Voltron <cryptobotmachine@gmail.com>
2018-08-15 21:56:23 -0400

    coin list drawer height wasnt set properly #752314468555221

--- app/javascript/App/scrollHelper.js
+++ app/javascript/App/scrollHelper.js
@@ -28,8 +28,8 @@ export default (element) => {
 
   if (coinListWrap) coinListWrap.style.height = bodyTag + 'px'
 
-  // TODO: figure out how to calculate the height because the percentage evaluates to different amts on chrome and ff
-  // if (coinListDrawer) coinListDrawer.style.height = coinListWrap.offsetHeight + 'px'
+  if (coinListDrawer)
+    coinListDrawer.style.height = calculatedNewsListHeight + 'px'
 
   if (!!element) element.style.maxHeight = `${calculatedNewsListHeight}px`
 }
