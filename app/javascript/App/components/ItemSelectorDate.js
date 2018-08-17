dc35fb7d5a23f68db2929841be5d0bab632b2bd2
Voltron <cryptobotmachine@gmail.com>
2018-08-17 13:01:07 -0400

    style datepicker #781947022555657

--- app/javascript/App/components/ItemSelectorDate.js
+++ app/javascript/App/components/ItemSelectorDate.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-datepicker'
+import moment from 'moment'
+// import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
 
 export default class ItemSelectorDates extends Component {
+
+  state = {
+      startDate: moment()
+  }
+
   selectedItems = () => this.props.selectedItems || []
 
   addFrom = (item) => {
@@ -9,11 +17,13 @@ export default class ItemSelectorDates extends Component {
     items.publishedSince = item.currentTarget.value
     this.props.onChange(items)
   }
+
   addTo = (item) => {
     let items = this.selectedItems()
     items.publishedUntil = item.currentTarget.value
     this.props.onChange(items)
   }
+
   remove = (item) => {
     let items = this.selectedItems().categories
     items = items.filter((c) => JSON.stringify(c) !== JSON.stringify(item))
@@ -41,32 +51,39 @@ export default class ItemSelectorDates extends Component {
     return (
       <div className="item-selector-alt nh1 nt1">
         <div className="pv4">
-          <DatePicker />
-
-          <DatePicker />
           <div
             className="w-50 dib"
             style={!window.isMobile ? { maxWidth: '50%' } : {}}
           >
             <label className="f6">Start Date</label>
-            <input
-              type="date"
+            <DatePicker
               className="from"
               onChange={this.addFrom.bind(this)}
-              value={publishedSince}
+              selected={this.state.startDate}
             />
+            {/* <input */}
+            {/*   type="date" */}
+            {/*   className="from" */}
+            {/*   onChange={this.addFrom.bind(this)} */}
+            {/*   value={publishedSince} */}
+            {/* /> */}
           </div>
           <div
             className="w-50 dib"
             style={!window.isMobile ? { maxWidth: '50%' } : {}}
           >
             <label className="f6">End Date</label>
-            <input
-              type="date"
+            <DatePicker
               className="to"
               onChange={this.addTo.bind(this)}
-              value={publishedUntil}
+              selected={this.state.startDate}
             />
+            {/* <input */}
+            {/*   type="date" */}
+            {/*   className="to" */}
+            {/*   onChange={this.addTo.bind(this)} */}
+            {/*   value={publishedUntil} */}
+            {/* /> */}
           </div>
         </div>
       </div>
