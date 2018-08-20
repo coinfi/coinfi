fad73e1e7004aa8e1134554c41a373028d5fac00
Voltron <cryptobotmachine@gmail.com>
2018-08-20 07:38:17 -0400

    stub select menu with fake data until api ready

--- app/javascript/App/bundles/ExchangeListings/components/ListingFilterQuoteSymbolField.tsx
+++ app/javascript/App/bundles/ExchangeListings/components/ListingFilterQuoteSymbolField.tsx
@@ -1,5 +1,10 @@
-import React, { Component } from 'react'
+import * as React from 'react'
 import AsyncSelect from 'react-select/lib/Async'
+import { colourOptions } from './docs/data'
+
+type State = {
+  inputValue: string,
+};
 
 const filterColors = (inputValue: string) =>
   colourOptions.filter(i =>
@@ -13,17 +18,19 @@ const promiseOptions = inputValue =>
     }, 1000);
   });
 
-class ListingFilterQuoteSymbolField extends Component {
+class ListingFilterQuoteSymbolField extends React.Component {
+      state = { inputValue: '' };
+ handleInputChange = (newValue: string) => {
+    const inputValue = newValue.replace(/\W/g, '');
+    this.setState({ inputValue });
+    return inputValue;
+  };
   render() {
   return (
     <div className="pb3">
       <h4 className="mb2 f5">Quote Symbol</h4>
-      <AsyncSelect
-        isMulti
-        cacheOptions
-        defaultOptions
-        loadOptions={promiseOptions}
-      />
+        <AsyncSelect isMulti cacheOptions defaultOptions loadOptions={promiseOptions} />
+
     </div>
   )
 }
