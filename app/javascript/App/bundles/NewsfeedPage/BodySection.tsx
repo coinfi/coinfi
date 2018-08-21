11a44bf2cb7f9384f8e16466f791d18acc678fdd
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2018-08-21 13:48:23 +0500

    Add news details fetching to NewsBody, move TwitterBody to NewsBody

--- app/javascript/App/bundles/NewsfeedPage/BodySection.tsx
+++ app/javascript/App/bundles/NewsfeedPage/BodySection.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
 import NewsBody from './NewsBody'
 import CoinBody from '../common/components/CoinBody'
-import TwitterBody from './TwitterBody'
 import Tips from './Tips'
-import LoadingIndicator from '../../components/LoadingIndicator';
-import { ContentType, NewsItem } from './types';
+import { ContentType } from './types';
 import { Coin, User } from '../common/types';
 
-import { getDomainType } from '../../lib/utils/url';
-
 interface Props {
-  newsItem?: NewsItem,
+  newsItemId?: string,
   coinInfo?: Coin,
   contentType: ContentType,
   closeTips: Function,
@@ -23,19 +19,7 @@ const BodySection = (props: Props) => {
   }
 
   if (props.contentType === "news") {
-    if(!props.newsItem) {
-      return (
-        <div className="pa3 tc mt4">
-          <LoadingIndicator />
-        </div>
-      ) 
-    }
-
-    if (getDomainType(props.newsItem.url) === "twitter") {
-      return <TwitterBody newsItem={props.newsItem} />
-    }
-
-    return <NewsBody newsItem={props.newsItem} />;
+    return <NewsBody newsItemId={props.newsItemId} />;
   }
 
   if (props.contentType === 'coin') return <CoinBody coin={props.coinInfo} user={props.user} />
