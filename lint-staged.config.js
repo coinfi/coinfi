cb60b28a0342be673f5598827f2c0715df4c9982
Han Chang <szu.han.chang@gmail.com>
2018-08-21 16:08:25 +0800

    Add tslint to lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   linters: {
+    '**/*.+(ts|tsx)': ['tslint --fix', 'prettier --write', 'git add'],
     '**/*.+(js|jsx|vue)': ['eslint --fix', 'prettier --write', 'git add'],
     '**/*.+(css|sass|less|scss|json|graphql)': ['prettier --write', 'git add'],
   },
