847c19765e4fdc597f7112aea40b1c3d27ff7dcb
Han Chang <szu.han.chang@gmail.com>
2018-08-21 17:29:45 +0800

    Remove FilterTags for simplicity #787851349369306

--- app/javascript/App/components/NewsfeedPage/NewsListHeader.js
+++ app/javascript/App/components/NewsfeedPage/NewsListHeader.js
@@ -1,19 +1,11 @@
 import React, { Fragment } from 'react'
 import SearchCoins from '../SearchCoins'
 import FilterPanel from '../FilterPanel'
-import FilterTags from '../FilterTags'
 import FilterBar from '../../bundles/common/components/FilterPanel/FilterBar'
 import CoinTipsTab from '../../bundles/common/components/CoinTipsTab'
 
 const NewsListHeader = (props) => {
-  const {
-    enableUI,
-    currentUI,
-    coins,
-    feedSources,
-    activeFilters,
-    newsfeedTips,
-  } = props
+  const { enableUI, currentUI, coins, feedSources, newsfeedTips } = props
 
   const toggleFilters = () =>
     enableUI('filterPanel', { fullScreen: window.isMobile })
@@ -32,12 +24,6 @@ const NewsListHeader = (props) => {
       <FilterBar {...props} toggleFilterPanel={toggleFilters}>
         <SearchCoins {...props} />
       </FilterBar>
-      {activeFilters.size > 0 && (
-        <div className="pa3 f6 b--b bg-athens flex items-center">
-          <span className="mr2">Viewing by:</span>
-          <FilterTags {...props} toggleFilterPanel={toggleFilters} />
-        </div>
-      )}
       {currentUI('filterPanel') && (
         <FilterPanel {...props} filterData={{ coins, feedSources }} />
       )}
