c0e32070abf2b55f8786019a755392bee3077772
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2018-08-29 12:45:04 +0500

    Fix image imports, disable no-var-require rule

--- app/javascript/App/bundles/NewsfeedPage/NewsListItem.tsx
+++ app/javascript/App/bundles/NewsfeedPage/NewsListItem.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import timeago from 'timeago.js'
 import CoinTags from '../common/components/CoinTags'
 import BulletSpacer from '../../components/BulletSpacer'
-import { slugify } from '../../lib/utils/slugify';
+import { slugify } from '../../lib/utils/slugify'
 
-const twitterLogo = require('../../../images/logo-twitter.svg');
-const linkLogo = require('../../../images/logo-link.svg');
-const redditLogo = require('../../../images/logo-reddit.svg');
+const twitterLogo = require('../../images/logo-twitter.svg')
+const linkLogo = require('../../images/logo-link.svg')
+const redditLogo = require('../../images/logo-reddit.svg')
 
-import classNames from 'classnames';
+import classNames from 'classnames'
 
 const readNewsHandler = (newsItem) => {
   const newsId = newsItem.id
@@ -29,10 +29,12 @@ const readNewsHandler = (newsItem) => {
 }
 
 const Title = styled.h4`
-  ${({ hasRead }: any) => hasRead && `
+  ${({ hasRead }: any) =>
+    hasRead &&
+    `
     color: #999;
-  `}
-`;
+  `};
+`
 
 const NewsListItem = (props) => {
   const {
@@ -43,10 +45,12 @@ const NewsListItem = (props) => {
     hasRead,
   } = props
 
-  let bgFoam = false;
+  let bgFoam = false
   if (activeEntity) {
     const { type, id } = activeEntity
-    if (type === 'newsItem' && id === newsItem.id) bgFoam = true; 
+    if (type === 'newsItem' && id === newsItem.id) {
+      bgFoam = true
+    }
   }
 
   const url = new URL(newsItem.url)
@@ -63,48 +67,65 @@ const NewsListItem = (props) => {
       )}
       style={{ height: props.height || 'auto' }}
     >
-        <div data-heap="news-click-on-news-item" className="pa-default">
-          <Link
-            to={`/news/${newsItem.id}/${slugify(newsItem.title)}`}
-            onClick={() => {
-              readNewsHandler(newsItem)
-              if (
-                document.querySelector('.selected-news-content') &&
-                document.querySelector('.selected-news-content').parentNode
-              )
-                document.querySelector(
-                  '.selected-news-content',
-                  // @ts-ignore
-                ).parentNode.scrollTop = 0
-            }}
-          >
-            <Title hasRead={hasRead}>
-              {newsItemTitle}
-            </Title>
-          </Link>
-          <div className="flex justify-between flex-wrap">
-            <div className="f6 silver">
-              {url.hostname === 'twitter.com' && (
+      <div data-heap="news-click-on-news-item" className="pa-default">
+        <Link
+          to={`/news/${newsItem.id}/${slugify(newsItem.title)}`}
+          onClick={() => {
+            readNewsHandler(newsItem)
+            if (
+              document.querySelector('.selected-news-content') &&
+              document.querySelector('.selected-news-content').parentNode
+            ) {
+              document.querySelector(
+                '.selected-news-content',
+                // @ts-ignore
+              ).parentNode.scrollTop = 0
+            }
+          }}
+        >
+          <Title hasRead={hasRead}>{newsItemTitle}</Title>
+        </Link>
+        <div className="flex justify-between flex-wrap">
+          <div className="f6 silver">
+            {url.hostname === 'twitter.com' && (
+              <>
+                <span className="mr2">
+                  <img src={twitterLogo} style={{ height: 11 }} />
+                </span>
+                <a
+                  href={`https://twitter.com/${url.pathname.split('/')[1]}`}
+                  target="_blank noopener noreferrer"
+                  rel="nofollow"
+                  className="dib silver"
+                >
+                  {`@${url.pathname.split('/')[1]}`}
+                </a>
+                <BulletSpacer />
+                {timeago().format(newsItem.feed_item_published_at)}
+              </>
+            )}
+            {url.hostname === 'www.reddit.com' && (
+              <>
+                <span className="mr2">
+                  <img src={redditLogo} style={{ height: 12 }} />
+                </span>
+                <a
+                  href={newsItem.url}
+                  target="_blank noopener noreferrer"
+                  rel="nofollow"
+                  className="dib silver"
+                >
+                  {`/r/${url.pathname.split('/')[2]}`}
+                </a>
+                <BulletSpacer />
+                {timeago().format(newsItem.feed_item_published_at)}
+              </>
+            )}
+            {url.hostname !== 'twitter.com' &&
+              url.hostname !== 'www.reddit.com' && (
                 <>
                   <span className="mr2">
-                    <img src={twitterLogo} style={{ height: 11 }} />
-                  </span>
-                  <a
-                    href={`https://twitter.com/${url.pathname.split('/')[1]}`}
-                    target="_blank noopener noreferrer"
-                    rel="nofollow"
-                    className="dib silver"
-                  >
-                    {`@${url.pathname.split('/')[1]}`}
-                  </a>
-                  <BulletSpacer />
-                  {timeago().format(newsItem.feed_item_published_at)}
-                </>
-              )}
-              {url.hostname === 'www.reddit.com' && (
-                <>
-                  <span className="mr2">
-                    <img src={redditLogo} style={{ height: 12 }} />
+                    <img src={linkLogo} style={{ height: 9 }} />
                   </span>
                   <a
                     href={newsItem.url}
@@ -112,34 +133,16 @@ const NewsListItem = (props) => {
                     rel="nofollow"
                     className="dib silver"
                   >
-                    {`/r/${url.pathname.split('/')[2]}`}
+                    {url.hostname}
                   </a>
                   <BulletSpacer />
                   {timeago().format(newsItem.feed_item_published_at)}
                 </>
               )}
-              {url.hostname !== 'twitter.com' &&
-                url.hostname !== 'www.reddit.com' && (
-                  <>
-                    <span className="mr2">
-                      <img src={linkLogo} style={{ height: 9 }} />
-                    </span>
-                    <a
-                      href={newsItem.url}
-                      target="_blank noopener noreferrer"
-                      rel="nofollow"
-                      className="dib silver"
-                    >
-                      {url.hostname}
-                    </a>
-                    <BulletSpacer />
-                    {timeago().format(newsItem.feed_item_published_at)}
-                  </>
-                )}
-            </div>
-            <CoinTags {...props} itemWithCoinLinkData={newsItem} />
           </div>
+          <CoinTags {...props} itemWithCoinLinkData={newsItem} />
         </div>
+      </div>
     </div>
   )
 }
