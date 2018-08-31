e4ea87e00912ae6456cf6ad5cbe7b96d51ae4764
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2018-08-31 07:06:42 +0500

    Style CoinlistHeader tabs (#365)

--- app/javascript/App/bundles/common/components/CoinListHeader.js
+++ app/javascript/App/bundles/common/components/CoinListHeader.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import CoinListContext from '../../../contexts/CoinListContext'
+
+const Tab = styled.div`
+  padding: 0 !important;
+  margin: 0 !important;
+  flex: 1;
+  height: 60px;
+  align-items: flex-end !important;
+  display: flex !important;
+  justify-content: center !important;
+  padding-bottom: 12px !important;
+  border-bottom: ${(props) =>
+    props.selected ? '2px solid #23adf0' : '2px solid transparent'} !important;
+`
+
+const Link = styled.a`
+  cursor: pointer;
+  color: ${(props) =>
+    props.selected ? '#23adf0 !important' : '#555 !important'};
+  font-weight: 100;
+`
 
 class CoinListHeader extends Component {
   render() {
     return (
       <CoinListContext.Consumer>
         {(payload) => (
-          <div
-            className="pa3 b--b flex-none flex justify-between items-center bg-athens"
-            style={{ height: 60 }}
-          >
-            <a
+          <div className="b--b flex-none flex justify-between items-center bg-athens tabs">
+            <Tab
+              className="tab"
+              selected={!payload.isWatchlist}
               onClick={payload.showToplist}
-              className="f5"
-              data-heap="toplist-toggle"
             >
-              Toplist
-            </a>
-            <a
+              <Link data-head="toplist-toggle" selected={!payload.isWatchlist}>
+                Toplist
+              </Link>
+            </Tab>
+            <Tab
+              className="tab"
+              selected={payload.isWatchlist}
               onClick={payload.showWatchlist}
-              className="f5"
-              data-heap="watchlist-toggle"
             >
-              Watchlist
-            </a>
+              <Link data-head="watchlist-toggle" selected={payload.isWatchlist}>
+                Watchlist
+              </Link>
+            </Tab>
           </div>
         )}
       </CoinListContext.Consumer>
