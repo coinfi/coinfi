6414bfc76c01cfd6811a38ed80a0bd072fc414af
Jesse Hon <jesse@jessehon.com>
2018-09-03 15:58:55 +1000

    Add additional profiles to About page

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -29,9 +29,9 @@ class TeamMember < ActiveHash::Base
       linkedin: 'https://www.linkedin.com/in/jonney-liu-0453824/'
     },
     {
-      name: 'Alexander Svanevik',
+      name: 'Alex Svanevik',
       title: 'Chief Data Scientist',
-      bio: "Manager at Schibsted Media Group, 8+ years of experience leading a group of 7 data scientists to create business value from data. Previously a management consultant at BearingPoint as well as cofounder of Codeus Artificial Intelligence. Experienced crypto trader who runs ICO Whitelists.",
+      bio: "Alex Svanevik is Chief Data Scientist at Coinfi, a market intelligence platform for cryptoassets. He is the founder of ICOWhitelists.com, a website for discovering initial coin offerings (ICOs). Alex has 8+ years experience from data science, management consulting, and startups in Schibsted Media Group, BearingPoint, and Codeus. He holds an MSc in Artificial Intelligence from the University of Edinburgh.",
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671943/profile-photos/alexander-svanevik.jpg',
       linkedin: 'https://www.linkedin.com/in/alexander-svanevik-152198a/'
     },
@@ -76,6 +76,62 @@ class TeamMember < ActiveHash::Base
       bio: "Experienced Customer Relationship Manager and Researcher with an MBA in Entrepreneurship from Bentley University, MA. Passionate about new technology, transitioned from a background in Biotechnology (MSc) to cryptocurrency.",
       img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671949/profile-photos/tanushree-jain.jpg',
       linkedin: ''
+    },
+    {
+      name: 'Sharon Chen',
+      title: 'Head of Content',
+      bio: "Sharon Chen previously worked as the Head of Content in a fast-growing eCommerce startup in the health and wellness industry. She has also been running a content site for more than five years including two book publications. Armed with extensive experience and skill set in content marketing and operations at a Chinese Bitcoin exchange back in 2013, Sharon has joined forces with CoinFi, aiming to bring maximum exposure to the platform through content marketing.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/sharen-chen.jpg',
+      linkedin: 'https://www.linkedin.com/in/sharonchen1099/'
+    },
+    {
+      name: 'Eugene Khaimson',
+      title: 'Head of Algorithm Development',
+      bio: "",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/eugene-khaimson.jpg',
+      linkedin: ''
+    },
+    {
+      name: 'Wayne Lam',
+      title: 'Junior Product Manager',
+      bio: "Previously worked at a fast paced CPA start up that grew from $0 to $1 million in annual revenues within two years. Experienced in working with small & medium businesses from a wide array of industries providing financial clarity and business solutions. Avid equities and crypto investor.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/wayne-lam.jpg',
+      linkedin: 'https://www.linkedin.com/in/wlamhk/'
+    },
+    {
+      name: 'Evgeny Medvedev',
+      title: '',
+      bio: "Software architect and data engineer with significant experience in architecting, building, and maintaining scalable systems. AWS Certified Solutions architect. Has deep knowledge of Ethereum having written the Ethereum ETL ingest library.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/evgeny-medvedev.jpg',
+      linkedin: 'https://www.linkedin.com/in/evgemedvedev'
+    },
+    {
+      name: 'Sylvia Leung',
+      title: 'Office & Administrative Manager',
+      bio: "Sylvia Leung is the Office and Administrative Manager at CoinFi, supporting the CEO and the team in both internal and external operations for business. Sylvia has over 7 years’ experience in office administration, marketing, and events management in both information technology and financial services. ",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/sylvia-leung.jpg',
+      linkedin: 'https://www.linkedin.com/in/sylviakyleung/'
+    },
+    {
+      name: 'Jane Lippencott',
+      title: 'Head of Business Development',
+      bio: "Jane is Head of Business Development at CoinFi, where she focuses on strategic partnerships, integrations, and outreach. She first started working in blockchain in 2016 as a founding team member of ZenCash, an encrypted ecosystem building an on-chain governance module and a privacy-oriented DAG protocol. Jane is an active advisor of blockchain funds Origin-X Capital, Lumen Capital Partners, and Ezra New Economy Fund and early-stage startups Blockhous and Cord. She’s passionate about decentralized governance, use cases for reputation protocols, and impact investing.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/jane-lippencott.jpg',
+      linkedin: 'http://linkedin.com/in/janelippencott'
+    },
+    {
+      name: 'Jesse Hon',
+      title: 'Senior Software Engineer',
+      bio: "Full-stack engineer with 8+ years of professional development experience across web and mobile. Former developer on an investment management platform with over 159,000 client portfolios and £31.9B under direction. Co-founded a number of startups and experienced in leading development of fast-growing products.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/jesse-hon.jpg',
+      linkedin: 'https://www.linkedin.com/in/jesse-hon-8386972b/'
+    },
+    {
+      name: 'Kwa Jie Hao',
+      title: 'Data Scientist',
+      bio: "B.A. in Economics and East Asian Studies from Brown University and a M.S. in Data Science from the Barcelona Graduate School of Economics and previously interned at Telefonica Research. Passionate about machine learning and statistics and is looking forward to applying data science to crypto.",
+      img: 'https://res.cloudinary.com/coinfi/image/upload/v1522671946/profile-photos/kwa-jie-hao.jpg',
+      linkedin: 'https://www.linkedin.com/in/jie-hao-kwa-70bbb984/'
     },
   ]
 end
