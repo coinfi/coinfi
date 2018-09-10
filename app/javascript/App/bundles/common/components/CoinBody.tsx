a6dbc34188371b6af908d2b7d67cfaf57a93e4f2
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2018-09-10 17:09:38 +0500

    Remove comment

--- app/javascript/App/bundles/common/components/CoinBody.tsx
+++ app/javascript/App/bundles/common/components/CoinBody.tsx
@@ -109,7 +109,6 @@ class CoinBody extends React.Component<IProps, IState> {
           symbol={coinWithDetails.symbol}
           priceData={coinWithDetails.prices_data}
           annotations={coinWithDetails.news_data}
-          // isTradingViewVisible={activeEntity.type !== 'coin'} // Quesion Andrey: why checking type == coin here if is this component is used only with .type=coin ?
           isTradingViewVisible={true}
         />
       </div>
