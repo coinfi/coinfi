1d6c113d2835d16fd7432b42c3bde86452525787
Jesse Hon <jesse@jessehon.com>
2018-09-11 11:39:58 +0800

    Fix undefined `$launch_darkly` in test environment

--- config/initializers/launch_darkly.rb
+++ config/initializers/launch_darkly.rb
@@ -1,3 +1,3 @@
 # This is actually handled on production by config/puma.rb in the `on_worker_boot` section.
 # This configuration is just for localhost development.
-$launch_darkly = LaunchDarkly::LDClient.new(ENV.fetch("LAUNCHDARKLY_SDK_KEY")) if Rails.env.development?
+$launch_darkly = LaunchDarkly::LDClient.new(ENV.fetch("LAUNCHDARKLY_SDK_KEY")) if Rails.env.test? || Rails.env.development?
