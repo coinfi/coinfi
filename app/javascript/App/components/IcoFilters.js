9fa1addbd838f2e72c7bec4bdaed06eee19b05a6
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2018-09-13 23:37:29 +0500

    Add store wrapper, add IcoFilters to react on rails

--- app/javascript/App/components/IcoFilters.js
+++ app/javascript/App/components/IcoFilters.js
@@ -1,3 +1,8 @@
+import ReactOnRails from 'react-on-rails'
 import Filters from './Filters'
 import container from '../containers/icoFilters'
-export default container(Filters)
+import withStore from '../withStore'
+
+const IcoFiltersWithStore = withStore(container(Filters))
+
+ReactOnRails.register({ IcoFilters: IcoFiltersWithStore })
--- app/javascript/App/startup.js
+++ app/javascript/App/startup.js
@@ -1,3 +1,7 @@
+// react-router app
 import './index'
+
+// non-react-router components
 import './bundles/common/containers/NavUserContainer'
 import './components/CoinShow'
+import './components/IcoFilters'
