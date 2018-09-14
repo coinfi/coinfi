cf2654f0ecb908196c13bb1c50c6007567841fd6
Jesse Hon <jesse@jessehon.com>
2018-09-14 13:40:06 +0800

    Update social sources copy

--- app/javascript/App/components/FilterPanel/filterComponents/Social.js
+++ app/javascript/App/components/FilterPanel/filterComponents/Social.js
@@ -9,8 +9,7 @@ const Social = (props) => {
   return (
     <Fragment>
       <div className="pv2 f6">
-        Reddit and Twitter often has more noise than signal so we&apos;ve
-        disabled them by default, but you can enable them here.
+        {`If a coin does not have a lot of news coverage from our general sources, we'll automatically enable social sources for the coin.`}
       </div>
       <ToggleReddit
         {...props}
