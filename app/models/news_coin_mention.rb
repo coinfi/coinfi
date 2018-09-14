5701d3f1ae204472af43ee8a24de79456e224e46
Jesse Hon <jesse@jessehon.com>
2018-09-14 18:21:25 +0800

    Add tag scope to NewsCoinMentions

--- app/models/news_coin_mention.rb
+++ app/models/news_coin_mention.rb
@@ -1,4 +1,7 @@
 class NewsCoinMention < ApplicationRecord
   belongs_to :coin
   belongs_to :news_item
+
+  scope :human_tagged, -> { where(is_machine_tagged: false) }
+  scope :machine_tagged, -> { where(is_machine_tagged: true) }
 end
