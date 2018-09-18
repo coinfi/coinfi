fa93492c33f60e70bf812c1a1de5f75757a80197
Han Chang <szu.han.chang@gmail.com>
2018-09-18 16:56:12 +0800

    Fix up CoinFi News Tips Sign Up CTA #823481256129514

--- app/javascript/App/bundles/NewsfeedPage/Tips.tsx
+++ app/javascript/App/bundles/NewsfeedPage/Tips.tsx
@@ -106,32 +106,18 @@ export default ({ closeTips, loggedIn }: Props) => {
         </div>
 
         {!loggedIn && (
-          <div style={{ paddingTop: '15px' }} className="signup-cta-wrap">
-            <h2 className="tc mt0">Get the most out of CoinFi News</h2>
-            <div style={{ margin: '0 20px 20px', display: 'grid' }}>
-              <div
-                style={{
-                  fontSize: '.8rem',
-                  marginBottom: 20,
-                  marginTop: 10,
-                  textAlign: 'center',
-                }}
-              >
-                Be the first to know about the latest market moving news
-              </div>
-              <button
-                className="btn btn-blue btn-l w-100"
-                style={{
-                  fontWeight: 'bold',
-                  margin: 'auto',
-                  padding: '.8rem 2rem',
-                  textTransform: 'none',
-                }}
-                onClick={() => (window.location.href = '/sign_up')}
-              >
-                Sign up
-              </button>
+          <div className="ph3 tc signup-cta-wrap">
+            <h2 className="m0 pv3">Get the most out of CoinFi News!</h2>
+            <div className="f6">
+              Save coins into your Watchlist and be the first to know about the
+              latest market moving news.
             </div>
+            <button
+              className="btn btn-blue btn-l w-30-ns mv3 b ttn"
+              onClick={() => (window.location.href = '/register')}
+            >
+              Sign Up Now
+            </button>
           </div>
         )}
       </div>
