394340d0946ebbf9673d38392ce1cb57bf275e7f
jessehon <honkalong@gmail.com>
2018-09-18 18:05:36 +0800

    Remove unusued components #823305638351052 (#414)
    * Add react_on_rails gem & package
    
    * Init React on Rails
    
    * Hide script tag
    
    * Rewrite App entry point, add react on rails register for App and NavUserContainer
    
    * Move component imports into startup file, add CoinShow to react on rails, move window size functions into application entry
    
    * Add store wrapper, add IcoFilters to react on rails
    
    * Add FlashMessageList to react on rails
    
    * Add WatchButton to react on rails
    
    * Add CalendarPage to react on rails
    
    * Upgrade react_on_rails
    
    * Move all react on rails register calls to `startup.js`
    
    * Add comment for ReactOnRails.register
    
    * Remove ReactOnRails.register from WatchButton
    
    * Remove legacy NewsfeedPage component
    
    * Remove unused import of newsfeedContainer
    
    * Remove empty exchangeListings container
    
    * Remove unused components
    
    * Fix required build dependency
    
    * Move all typings into 'dependencies'

--- app/javascript/App/components/CoinShow/index.js
+++ app/javascript/App/components/CoinShow/index.js
@@ -6,7 +6,6 @@ import axios from 'axios'
 import SearchCoins from '../../bundles/common/components/SearchCoins'
 import CoinCharts from './../CoinCharts'
 import SectionHeader from './../shared/SectionHeader'
-import newsfeedContainer from './../../containers/newsfeed'
 import FundamentalsData from './FundamentalsData'
 import LinksData from './LinksData'
 
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,17 +3,12 @@ import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
 import CoinSummary from '../app/javascript/App/components/CoinSummary'
-import CoinDetail from '../app/javascript/App/components/CoinDetail'
-
-// import { BodySection } from '../app/javascript/App/components/NewsfeedPage/BodySection'
-// import { NewsBody } from '../app/javascript/App/components/NewsfeedPage/NewsBody'
 
 // import { bodySectionProps } from './bodySectionProps'
 
 // import styles from './tachy.css'
 
 storiesOf('Coin', module).add('summary', () => <CoinSummary />)
-storiesOf('Coin', module).add('detail', () => <CoinDetail />)
 
 /*
 storiesOf('News', module).add('body section', () => <BodySection bucket={bodySectionProps}  />);
