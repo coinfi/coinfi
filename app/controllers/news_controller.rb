29104336ca895f173912098218ac4f87ae5d4e94
Jesse Hon <jesse@jessehon.com>
2018-09-18 15:34:09 +0800

    Set class instead of id to manage full screen behavior

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,13 @@
 class NewsController < ApplicationController
+  before_action :set_body_class
+
   def index
     return render_404 unless has_news_feature?
+  end
+
+  protected
+
+  def set_body_class
+    @body_class = 'page page--fullscreen'
   end
 end
