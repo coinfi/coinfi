6a76855d5f90fb495e9f43f2939f95320d4f3791
Jesse Hon <jesse@jessehon.com>
2018-09-18 16:41:36 +0800

    Fix layout sections on Firefox

--- app/javascript/App/components/LayoutDesktop.js
+++ app/javascript/App/components/LayoutDesktop.js
@@ -1,24 +1,48 @@
 import React from 'react'
+import { withStyles, createStyles } from '@material-ui/core/styles'
 
-export default function({
+const styles = (theme) =>
+  createStyles({
+    root: {},
+    section: {
+      maxHeight: '100%',
+    },
+  })
+
+const LayoutDesktop = ({
+  classes,
   leftSection,
   centerSection,
   rightSection,
   ...props
-}) {
+}) => {
   return (
     <div className="flex flex-column flex-auto column-wrap">
       <div className="row no-gutter flex-auto bg-white">
-        <div className="col-xs-2 relative flex flex-column b--l">
+        <div
+          className={`${
+            classes.section
+          } col-xs-2 relative flex flex-column b--l`}
+        >
           {leftSection}
         </div>
-        <div className="col-xs-5 relative flex flex-column b--l">
+        <div
+          className={`${
+            classes.section
+          } col-xs-5 relative flex flex-column b--l`}
+        >
           {centerSection}
         </div>
-        <div className="col-xs-5 relative overflow-y-auto b--l b--r">
+        <div
+          className={`${
+            classes.section
+          } col-xs-5 relative overflow-y-auto b--l b--r`}
+        >
           {rightSection}
         </div>
       </div>
     </div>
   )
 }
+
+export default withStyles(styles)(LayoutDesktop)
