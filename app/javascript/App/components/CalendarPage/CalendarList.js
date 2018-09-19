3bc365b7a14a11dcb09b3e6d2e18c5f924b83719
Jesse Hon <jesse@jessehon.com>
2018-09-19 09:17:45 +0800

    Remove unnecessary resize

--- app/javascript/App/components/CalendarPage/CalendarList.js
+++ app/javascript/App/components/CalendarPage/CalendarList.js
@@ -23,7 +23,6 @@ class CalendarList extends Component {
       this.setState({ initialRender: false })
     }, 60000)
     this.mountOnScrollHandler()
-    document.querySelector('#calendar').style.height = `${window.outerHeight}px`
   }
 
   componentDidUpdate() {
