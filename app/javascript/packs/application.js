35a8e2a73b6410af0bfa209c98b3ac029114c308
Jesse Hon <jesse@jessehon.com>
2018-09-20 10:03:03 +0800

    Remove leftover `application.js` pack

--- app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill'
-
-import 'jquery' // jQuery has been configured to be globally available
-
-// Non-React code
-import '../modules/navigation'
-
-// React
-import '../App/startup'
