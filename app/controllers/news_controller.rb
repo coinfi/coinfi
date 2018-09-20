a88c7276fb88340c06da40ad63d4a8b668cba330
Jesse Hon <jesse@jessehon.com>
2018-09-20 15:05:02 +0800

    Return initial news items using query from NewsController

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,22 @@
 class NewsController < ApplicationController
+  before_action :check_permissions
   before_action :set_body_class
   before_action :set_view_data
 
   def index
-    return render_404 unless has_news_feature?
+    @news_items_data = serialize_news_items(
+      NewsItems::WithFilters.call()
+        .includes(:coins, :news_categories)
+        .order_by_published
+        .limit(25)
+    )
   end
 
   protected
+
+  def check_permissions
+    return render_404 unless has_news_feature?
+  end
 
   def set_view_data
     @feed_sources = (FeedSource.feed_types - ['general']) +
@@ -14,12 +24,12 @@ class NewsController < ApplicationController
     @top_coin_slugs = Coin.top(5).pluck(:slug)
     @categories = NewsCategory.pluck(:name)
 
-    @top_coins_data = coinlist_serializer(
+    @top_coins_data = serialize_coins(
       Rails.cache.fetch("coins/toplist", expires_in: 1.hour) do
         Coin.order(:ranking).limit(20)
       end
     )
-    @watched_coins_data = coinlist_serializer(
+    @watched_coins_data = serialize_coins(
       current_user.watchlist.coins.order(:ranking)
     ) if current_user
   end
@@ -28,10 +38,31 @@ class NewsController < ApplicationController
     @body_class = 'page page--fullscreen'
   end
 
-  def coinlist_serializer(coins)
+  def serialize_coins(coins)
     coins.as_json(
       only: %i[id name symbol slug price_usd],
       methods: %i[market_info]
     )
+  end
+
+  def serialize_news_items(news_items)
+    data = news_items.as_json(
+      only: %i[id title summary feed_item_published_at updated_at url content],
+      methods: %i[tag_scoped_coin_link_data categories]
+    )
+    format_item = Proc.new do |item, *args|
+      item
+        .except('tag_scoped_coin_link_data')
+        .merge({
+          coin_link_data: item['tag_scoped_coin_link_data'],
+        })
+    end
+
+    # Handle both hashes and arrays of hashes
+    if (data.kind_of?(Array))
+      formatted_data = data.map(&format_item)
+    else
+      formatted_data = format_item.call(data)
+    end
   end
 end
