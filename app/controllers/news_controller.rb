c34261932945c8ab8fb5c4ea8e5c9a03011dfaf6
Jesse Hon <jesse@jessehon.com>
2018-09-20 10:47:11 +0800

    Move Newsfeed view data back into model

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,11 +1,19 @@
 class NewsController < ApplicationController
   before_action :set_body_class
+  before_action :set_view_data
 
   def index
     return render_404 unless has_news_feature?
   end
 
   protected
+
+  def set_view_data
+    @feed_sources = (FeedSource.feed_types - ['general']) +
+      FeedSource.where(feed_type: 'general').pluck(:site_hostname)
+    @top_coin_slugs = Coin.top(5).pluck(:slug)
+    @categories = NewsCategory.pluck(:name)
+  end
 
   def set_body_class
     @body_class = 'page page--fullscreen'
