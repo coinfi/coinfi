df2bbd27ee7a1aca68667bb9cd0407b1630843aa
Jesse Hon <jesse@jessehon.com>
2018-09-24 15:26:09 +0800

    Enable server side rendering

--- app/javascript/App/serverStartup.js
+++ app/javascript/App/serverStartup.js
@@ -5,9 +5,17 @@ import ServerApp from './ServerApp'
 
 // non-react-router components
 import CoinShow from './components/CoinShow'
+import SignalExamplePanel from './bundles/signalsPage/SignalExamplePanel'
+import SignalFaqPanel from './bundles/signalsPage/SignalFaqPanel'
+import SignalPopoverText from './bundles/signalsPage/SignalPopoverText'
+import SignalTeamMember from './bundles/signalsPage/SignalTeamMember'
 
 // Register the components so they can be rendered from Rails
 ReactOnRails.register({
   CoinShow,
+  SignalExamplePanel,
+  SignalFaqPanel,
+  SignalPopoverText,
+  SignalTeamMember,
   App: ServerApp,
 })
