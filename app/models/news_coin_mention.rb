2d04b62baa15257c459bb0db250b92a181ccd001
Han Chang <szu.han.chang@gmail.com>
2018-09-25 12:21:07 +0800

    Simplify NewsCoinMention#default_tagged logic #836042970925458

--- app/models/news_coin_mention.rb
+++ app/models/news_coin_mention.rb
@@ -4,33 +4,6 @@ class NewsCoinMention < ApplicationRecord
 
   scope :human_tagged, -> { where(is_machine_tagged: false) }
   scope :machine_tagged, -> { where(is_machine_tagged: true) }
-  scope :combo_tagged, -> {
-    joins(
-      <<-SQL
-        LEFT OUTER JOIN (
-              SELECT
-                news_item_id
-              FROM
-                news_coin_mentions
-              WHERE
-                is_machine_tagged = false
-              GROUP BY
-                news_item_id
-            ) human_tagged_news_coin_mentions ON
-          human_tagged_news_coin_mentions.news_item_id = news_coin_mentions.news_item_id
-      SQL
-    )
-    .where(
-      <<-SQL
-        ( human_tagged_news_coin_mentions.news_item_id = news_coin_mentions.news_item_id AND
-          news_coin_mentions.is_machine_tagged = FALSE
-        ) OR
-        ( human_tagged_news_coin_mentions.news_item_id IS NULL AND
-          news_coin_mentions.is_machine_tagged = TRUE
-        )
-      SQL
-    )
-  }
 
   def self.default_tagged
     case ENV.fetch('NEWS_COIN_MENTION_TAG_SCOPE')
@@ -39,7 +12,7 @@ class NewsCoinMention < ApplicationRecord
     when 'machine'
       self.machine_tagged
     when 'combo'
-      self.combo_tagged
+      self.human_tagged.or(self.machine_tagged)
     else
       raise "Invalid NEWS_COIN_MENTION_TAG_SCOPE environment variable"
     end
