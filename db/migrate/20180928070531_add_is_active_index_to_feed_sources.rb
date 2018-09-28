c7284a0360b7dc865b933345f953c8a2987777b2
Jesse Hon <jesse@jessehon.com>
2018-09-28 15:16:54 +0800

    Apply migration changes suggested by `strong_migrations`

--- db/migrate/20180928070531_add_is_active_index_to_feed_sources.rb
+++ db/migrate/20180928070531_add_is_active_index_to_feed_sources.rb
@@ -1,5 +1,7 @@
 class AddIsActiveIndexToFeedSources < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
+
   def change
-    add_index :feed_sources, :is_active
+    add_index :feed_sources, :is_active, algorithm: :concurrently
   end
 end
