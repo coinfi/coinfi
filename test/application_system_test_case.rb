8e3744827709fe88fc65435b96628f4799fae900
Jesse Hon <jesse@jessehon.com>
2018-10-02 18:40:48 +0800

    Rely on `ReactOnRails::TestHelper.ensure_assets_compiled` to compile instead of webpacker

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { args: %w[headless disable-gpu] }
 
-  def setup
+  setup do
     ReactOnRails::TestHelper.ensure_assets_compiled
   end
 end
