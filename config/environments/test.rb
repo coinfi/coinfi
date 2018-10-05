0e4b2aac552e777848b88f3f64e6b689d6a63aca
Jesse Hon <jesse@jessehon.com>
2018-10-05 16:11:42 +0800

    Setup environment for system testing React frontend

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,8 +25,9 @@ Rails.application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # # Disable request forgery protection in test environment.
+  # config.action_controller.allow_forgery_protection = false
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,28 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "factory_bot_rails"
+require 'minitest/autorun'
+require 'minitest/stub_any_instance'
+
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.logger = Rails.logger
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include FactoryBot::Syntax::Methods
+
+  self.use_transactional_tests = false
+
+  ActiveRecord::Migration.check_pending!
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
 end
