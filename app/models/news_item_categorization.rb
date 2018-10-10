2d3702f64a73dcff66c88adeab4c531d9257fa89
Wayne Lam <wayne.lam@coinfi.com>
2018-10-10 17:42:24 +0800

    Change /pending view so machine tagged categories will not show up

--- app/models/news_item_categorization.rb
+++ app/models/news_item_categorization.rb
@@ -2,5 +2,7 @@ class NewsItemCategorization < ApplicationRecord
   belongs_to :news_item
   belongs_to :news_category
 
+  scope :no_category, -> { where(news_category_id: nil)}
+
   validates_uniqueness_of :news_category, :scope => :news_item
 end
