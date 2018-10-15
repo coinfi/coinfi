d8fc52fb0ca7633de1c0f59c3653556035acbbbd
Jesse Hon <jesse@jessehon.com>
2018-10-15 14:04:50 +0800

    Fix fetching token_sale attribute value

--- app/controllers/api/signals_telegram_bot_controller.rb
+++ app/controllers/api/signals_telegram_bot_controller.rb
@@ -15,9 +15,9 @@ class Api::SignalsTelegramBotController < ApiController
     users = User.where("(token_sale->>'signals_telegram_bot_chat_id') IS NOT NULL").select(:token_sale)
     subscribers = users.map do |user|
       {
-        telegram_username: user.token_sale.telegram_username,
-        chat_id: user.token_sale.signals_telegram_bot_chat_id,
-        started_at: user.token_sale.signals_telegram_bot_started_at,
+        telegram_username: user.token_sale['telegram_username'],
+        chat_id: user.token_sale['signals_telegram_bot_chat_id'],
+        started_at: user.token_sale['signals_telegram_bot_started_at'],
       }
     end
 
