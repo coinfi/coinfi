026de50daa64e3733c4af0da7c69385627dfa86c
Jesse Hon <jesse@jessehon.com>
2018-10-15 18:26:51 +0800

    Password protect routes

--- app/controllers/api/signals_telegram_bot_controller.rb
+++ app/controllers/api/signals_telegram_bot_controller.rb
@@ -1,5 +1,6 @@
 class Api::SignalsTelegramBotController < ApiController
   skip_before_action :verify_authenticity_token
+  http_basic_authenticate_with name: ENV.fetch('SIGNALS_TELEGRAM_BOT_API_USERNAME'), password: ENV.fetch('SIGNALS_TELEGRAM_BOT_API_PASSWORD')
 
   def register
     form = SignalsTelegramBotRegistrationForm.new(register_params)
