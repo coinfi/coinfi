6144d8ffdfbfd481ee80aa028b72b6a2011949bb
Anthony Tsang <tsang@antho.com>
2018-10-24 09:10:44 +0900

    Add new table to keep track of calculated total market cap.

---
+++ app/models/market_cap.rb
@@ -0,0 +1,2 @@
+class MarketCap < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004013839) do
+ActiveRecord::Schema.define(version: 20181024000236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -339,6 +339,13 @@ ActiveRecord::Schema.define(version: 20181004013839) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "market_caps", force: :cascade do |t|
+    t.decimal "total_market_cap", precision: 18, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at"], name: "index_market_caps_on_created_at"
   end
 
   create_table "news_categories", force: :cascade do |t|
