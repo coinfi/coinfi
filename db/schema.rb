5914bd199055337a40572e192fb623233d99bbdf
Anthony Tsang <tsang@antho.com>
2018-10-26 10:52:02 +0900

    Add news_item_id index to news_item_raws table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025032258) do
+ActiveRecord::Schema.define(version: 20181026104531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -375,6 +375,7 @@ ActiveRecord::Schema.define(version: 20181025032258) do
     t.boolean "is_processed", default: false
     t.integer "news_item_id"
     t.boolean "was_replaced_by_an_update"
+    t.index ["news_item_id"], name: "index_news_item_raws_on_news_item_id"
   end
 
   create_table "news_items", force: :cascade do |t|
