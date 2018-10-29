e66d263e3266d2c3274f7919982f2a2f5f19a2b5
Jesse Hon <jesse@jessehon.com>
2018-10-29 10:03:37 +0800

    Fix model and tests

--- app/models/staked_cofi_transaction.rb
+++ app/models/staked_cofi_transaction.rb
@@ -2,7 +2,10 @@ class StakedCofiTransaction < ApplicationRecord
   belongs_to :user
 
   def txn_quantity
-    quantity_text = txn_value.insert(-1 * txn_token_decimal - 1, '.')
-    BigDecimal quantity_text
+    return txn_value if txn_token_decimal = 0
+
+    padded_txn_value = txn_value.rjust(txn_token_decimal, "0")
+    txn_quantity_text = padded_txn_value.insert(-1 * txn_token_decimal - 1, '.')
+    BigDecimal txn_quantity_text
   end
 end
--- test/models/staked_cofi_transaction_test.rb
+++ test/models/staked_cofi_transaction_test.rb
@@ -1,11 +1,47 @@
 require 'test_helper'
 
 class StakedCofiTransactionTest < ActiveSupport::TestCase
-  test 'transaction_quantity is correct' do
+  test 'txn_quantity is correct' do
     transaction = build(
       :staked_cofi_transaction,
       txn_value: '20000000000000000000000',
       txn_token_decimal: 18
+    )
+    assert_equal transaction.txn_quantity, 20000
+  end
+
+  test 'txn_quantity is correct when txn_value is small' do
+    transaction = build(
+      :staked_cofi_transaction,
+      txn_value: '1',
+      txn_token_decimal: 18
+    )
+    assert_equal transaction.txn_quantity, 10 ** -18
+  end
+
+  test 'txn_quantity is correct when txn_value is 0' do
+    transaction = build(
+      :staked_cofi_transaction,
+      txn_value: '0',
+      txn_token_decimal: 18
+    )
+    assert_equal transaction.txn_quantity, 0
+  end
+
+  test 'txn_quantity is correct when txn_token_decimal is 0' do
+    transaction = build(
+      :staked_cofi_transaction,
+      txn_value: '20000',
+      txn_token_decimal: 0
+    )
+    assert_equal transaction.txn_quantity, 20000
+  end
+
+  test 'txn_quantity is correct when txn_value is 0 and txn_token_decimal is 0' do
+    transaction = build(
+      :staked_cofi_transaction,
+      txn_value: '0',
+      txn_token_decimal: 0
     )
     assert_equal transaction.txn_quantity, 20000
   end
