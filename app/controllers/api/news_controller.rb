24c63d64e457144e381fc7b37efb1208278d433b
Anthony Tsang <tsang@antho.com>
2018-10-31 08:31:02 +0900

    Move front page where clause from scope to actual query.

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -11,14 +11,14 @@ class Api::NewsController < ApiController
       news_items = nil
 
       if params[:frontPage].present? # For Front page (public; no feature flag)
-        news_items = NewsItem.published.front_page
+        news_items = NewsItem.published.where(feed_source: FeedSource.coindesk.or(FeedSource.cointelegraph))
           .includes(:coins, :news_categories)
           .order_by_published
           .limit(5)
       else # For newsfeed (keep feature flag)
         news_feature_response = detect_news_feature
         return news_feature_response unless news_feature_response.nil?
-        
+
         news_items = apply_news_feed_filters(params)
       end
 
