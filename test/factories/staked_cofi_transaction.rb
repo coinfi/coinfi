e2d047f26925a191c889d23ba5db2d3f4a8c3c18
Jesse Hon <jesse@jessehon.com>
2018-10-30 14:34:52 +0800

    Create test for associating staked COFI transactions

--- test/factories/staked_cofi_transaction.rb
+++ test/factories/staked_cofi_transaction.rb
@@ -6,7 +6,9 @@ FactoryBot.define do
     txn_timestamp { "1538563239" }
     txn_hash { "0x#{Faker::Crypto.sha256}" }
     txn_block_hash { "0x#{Faker::Crypto.sha256}" }
-    txn_from { "0x#{Faker::Crypto.sha1}" }
+    txn_from {
+      user&.token_sale&.fetch('staked_ethereum_address', nil) || "0x#{Faker::Crypto.sha1}"
+    }
     txn_to { "0x#{Faker::Crypto.sha1}" }
     txn_value { Faker::Number.between(1 * 10 ** 18, 10000 * 10 ** 18) }
     txn_token_decimal { 18 }
