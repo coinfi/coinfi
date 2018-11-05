7cf45048d315d41621de9018ad69251758f7a562
Han Chang <szu.han.chang@gmail.com>
2018-11-05 10:18:19 +0800

    Implement distribute_reads call to slow performing queries #895702502268589

--- app/controllers/icos_controller.rb
+++ app/controllers/icos_controller.rb
@@ -2,13 +2,16 @@ class IcosController < ApplicationController
   ICOS_PER_PAGE = 100
 
   def index
-    @status = params[:status]
-    redirect_to "/icos/upcoming" && return unless Coin::ICO_STATUSES.include?(@status)
+    distribute_reads(max_lag: MAX_ACCEPTABLE_REPLICATION_LAG, lag_failover: true) do
+      @status = params[:status]
+      redirect_to "/icos/upcoming" && return unless Coin::ICO_STATUSES.include?(@status)
 
-    @coins = Coin.where(ico_status: @status)
-    @result_count_total = @coins.length
-    @coins = @coins.page(params[:page]).per(ICOS_PER_PAGE)
-    @result_count = @coins.length
+      @coins = Coin.where(ico_status: @status)
+      @result_count_total = @coins.length
+      @coins = @coins.page(params[:page]).per(ICOS_PER_PAGE)
+      @result_count = @coins.length
+    end
+
     set_meta_tags(
       title: "#{@status.capitalize} ICOs",
       keywords: "ico list, ico rating, ico alert, ico review, initial coin offering, initial coin offering list, ico initial coin offering"
