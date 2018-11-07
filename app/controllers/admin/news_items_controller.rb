7f69950ef1f3c6f3816b215a9d83396ddbdaad28
Han Chang <szu.han.chang@gmail.com>
2018-11-07 17:05:03 +0800

    Use distribute_reads for Administrate NewsItem queries #898425100970809

--- app/controllers/admin/news_items_controller.rb
+++ app/controllers/admin/news_items_controller.rb
@@ -11,11 +11,13 @@ module Admin
       respond_to do |format|
         format.html {
           page = Administrate::Page::Collection.new(dashboard)
-          resources = NewsItem.pending.no_category.where("feed_item_published_at > ?", 1.month.ago).order_by_published
-            .includes(:feed_source)
-            .page(params[:page])
-            .per(records_per_page)
-          render :index, locals: { page: page, resources: resources, search_term: search_term, show_search_bar: show_search_bar? }
+          distribute_reads(max_lag: 10.seconds, lag_failover: true) do
+            resources = NewsItem.pending.no_category.where("feed_item_published_at > ?", 1.month.ago).order_by_published
+              .includes(:feed_source)
+              .page(params[:page])
+              .per(records_per_page)
+            render :index, locals: { page: page, resources: resources, search_term: search_term, show_search_bar: show_search_bar? }
+          end
         }
         # TODO: Make this more performant - right now way too slow!
         # format.csv { send_data NewsItemCsvGenerator.to_csv(NewsItem.pending) }
@@ -26,23 +28,25 @@ module Admin
       respond_to do |format|
         format.html {
           page = Administrate::Page::Collection.new(dashboard)
-          resources = NewsItem.tagged.where("feed_item_published_at > ?", 1.month.ago).order_by_published
+          distribute_reads(max_lag: 10.seconds, lag_failover: true) do
+            resources = NewsItem.tagged.where("feed_item_published_at > ?", 1.month.ago).order_by_published
 
-          if params[:categories]
-            category_ids = params[:categories].split(',')
-            resources = resources.where(news_item_categorizations: { news_category_id: category_ids })
+            if params[:categories]
+              category_ids = params[:categories].split(',')
+              resources = resources.where(news_item_categorizations: { news_category_id: category_ids })
+            end
+
+            if params[:coins]
+              coin_ids = params[:coins].split(',')
+              resources = resources.where(news_coin_mentions: { coin_id: coin_ids })
+            end
+
+            resources = resources
+              .includes(:feed_source)
+              .page(params[:page])
+              .per(records_per_page)
+            render :index, locals: { page: page, resources: resources, search_term: search_term, show_search_bar: show_search_bar? }
           end
-
-          if params[:coins]
-            coin_ids = params[:coins].split(',')
-            resources = resources.where(news_coin_mentions: { coin_id: coin_ids })
-          end
-
-          resources = resources
-            .includes(:feed_source)
-            .page(params[:page])
-            .per(records_per_page)
-          render :index, locals: { page: page, resources: resources, search_term: search_term, show_search_bar: show_search_bar? }
         }
         format.csv { send_data NewsItemCsvGenerator.to_csv(NewsItem.tagged) }
       end
