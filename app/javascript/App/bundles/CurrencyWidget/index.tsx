412a091704d825f1c135eef8ced340e328cb1575
Anthony Tsang <tsang@antho.com>
2018-11-07 15:37:46 +0900

    Switch to react-cookies instead of context.

--- app/javascript/App/bundles/CurrencyWidget/index.tsx
+++ app/javascript/App/bundles/CurrencyWidget/index.tsx
@@ -1,22 +1,50 @@
 import * as React from 'react'
 import CurrencySelector from '~/bundles/common/components/CurrencySelector'
-import CurrencyContext, {
-  CurrencyContextType,
-} from '~/bundles/common/contexts/CurrencyContext'
+import { withCookies, Cookies } from 'react-cookie'
 
-const CurrencyWidget = () => {
-  return (
-    <CurrencyContext.Consumer>
-      {(payload: CurrencyContextType) => {
-        return (
-          <CurrencySelector
-            value={payload.currency}
-            onChange={payload.changeCurrency}
-          />
-        )
-      }}
-    </CurrencyContext.Consumer>
-  )
+interface Props {
+  cookies: Cookies
 }
 
-export default CurrencyWidget
+interface State {
+  currency: string
+}
+
+class CurrencyWidget extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+
+    const { cookies } = props
+    const currency = cookies.get('currency') || 'USD'
+    this.state = {
+      currency,
+    }
+  }
+
+  public componentDidUpdate(prevProps, prevState) {
+    const { cookies } = this.props
+    const currency = cookies.get('currency')
+
+    if (prevState.currency !== currency) {
+      this.setState({ currency })
+    }
+  }
+
+  public changeCurrency = (currency) => {
+    const { cookies } = this.props
+
+    cookies.set('currency', currency)
+    this.setState({ currency })
+  }
+
+  public render() {
+    return (
+      <CurrencySelector
+        value={this.state.currency}
+        onChange={this.changeCurrency}
+      />
+    )
+  }
+}
+
+export default withCookies(CurrencyWidget)
--- app/javascript/App/withRootProviders.tsx
+++ app/javascript/App/withRootProviders.tsx
@@ -3,10 +3,10 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
 import { DeviceProvider } from '~/bundles/common/contexts/DeviceContext'
 import { RailsProvider } from '~/bundles/common/contexts/RailsContext'
-import { CurrencyProvider } from '~/bundles/common/contexts/CurrencyContext'
 import JssProvider from 'react-jss/lib/JssProvider'
 import getOrCreateStylesContext from '~/getOrCreateStylesContext'
 import ClearJssServerSide from '~/ClearJssServerSide'
+import { CookiesProvider } from 'react-cookie'
 
 interface WithClientProvidersOptions {
   stylesNamespace?: string
@@ -24,27 +24,25 @@ const withRootProviders = (
     const stylesContext = getOrCreateStylesContext(stylesNamespace)
 
     return (
-      <JssProvider
-        registry={stylesContext.sheetsRegistry}
-        generateClassName={stylesContext.generateClassName}
-      >
-        <MuiThemeProvider
-          theme={theme}
-          sheetsManager={stylesContext.sheetsManager}
+      <CookiesProvider>
+        <JssProvider
+          registry={stylesContext.sheetsRegistry}
+          generateClassName={stylesContext.generateClassName}
         >
-          <ClearJssServerSide stylesNamespace={stylesNamespace}>
-            <RailsProvider railsContext={railsContext}>
-              <DeviceProvider {...railsContext.deviceProviderProps}>
-                <CurrencyProvider
-                  initialCurrency={railsContext.initialCurrency}
-                >
+          <MuiThemeProvider
+            theme={theme}
+            sheetsManager={stylesContext.sheetsManager}
+          >
+            <ClearJssServerSide stylesNamespace={stylesNamespace}>
+              <RailsProvider railsContext={railsContext}>
+                <DeviceProvider {...railsContext.deviceProviderProps}>
                   <TargetComponent {...props} />
-                </CurrencyProvider>
-              </DeviceProvider>
-            </RailsProvider>
-          </ClearJssServerSide>
-        </MuiThemeProvider>
-      </JssProvider>
+                </DeviceProvider>
+              </RailsProvider>
+            </ClearJssServerSide>
+          </MuiThemeProvider>
+        </JssProvider>
+      </CookiesProvider>
     )
   }
 
