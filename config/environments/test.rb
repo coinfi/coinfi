075721d2be8270abe8c6763fc68e1ad57bf8b116
Han Chang <szu.han.chang@gmail.com>
2018-11-13 13:20:24 +0800

    Remove test environment null_cache #905926531485284

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  # Make sure there is no cache
-  config.cache_store = :null_store
 end
