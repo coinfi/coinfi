34c2f8dfaa7102eb5912232a53b676e1215e7464
Han Chang <szu.han.chang@gmail.com>
2018-11-13 11:43:08 +0800

    Add tests for CMC -> NP switchover #905926531485284

--- test/application_integration_test.rb
+++ test/application_integration_test.rb
@@ -3,5 +3,14 @@ require "test_helper"
 class ApplicationIntegrationTest < ActionDispatch::IntegrationTest
   setup do
     ReactOnRails::TestHelper.ensure_assets_compiled
+
+    stub_request(:get, Regexp.new('api.coinmarketcap.com/v1/ticker')).
+      to_return(status: 200, body: File.new(Rails.root.join('test', 'fixtures', 'coinmarketcap-ticker.json')))
+
+    stub_request(:get, Regexp.new('coinmarketcap.northpole.ro/ticker.json')).
+      to_return(status: 200, body: File.new(Rails.root.join('test', 'fixtures', 'northpole-ticker.json')))
+
+    stub_request(:get, Regexp.new('postgrest.coinfi.com')).
+      to_return(status: 200)
   end
 end
