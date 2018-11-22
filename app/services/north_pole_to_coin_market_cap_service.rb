50a77ea7bf2d03574298f89695c194f5f7ce273a
Han Chang <szu.han.chang@gmail.com>
2018-11-22 11:08:52 +0800

    Gracefully degrade NorthPoleToCoinMarketCapService

--- app/services/north_pole_to_coin_market_cap_service.rb
+++ app/services/north_pole_to_coin_market_cap_service.rb
@@ -5,13 +5,23 @@ class NorthPoleToCoinMarketCapService
 
   # Converts NorthPole proxy data format into the CoinMarketCap format.
   def convert
-    market = @body["markets"][0]
-    {
-      "rank" => market["position"],
-      "price_usd" => market["price"]["usd"],
-      "price_btc" => market["price"]["btc"],
-      "24h_volume_usd" => market["volume24"]["usd"],
-      "market_cap_usd" => market["marketCap"]["usd"],
-    }
+    if @body["markets"]
+      market = @body["markets"][0]
+      {
+        "rank" => market.fetch("position", 0),
+        "price_usd" => market.dig("price", "usd") || 0,
+        "price_btc" => market.dig("price", "btc") || 0,
+        "24h_volume_usd" => market.dig("volume24", "usd") || 0,
+        "market_cap_usd" => market.dig("marketCap", "usd") || 0,
+      }
+    else # The slug passed in isn't valid on CMC...
+      {
+        "rank" => "Unknown",
+        "price_usd" => 0,
+        "price_btc" => 0,
+        "24h_volume_usd" => 0,
+        "market_cap_usd" => 0,
+      }
+    end
   end
 end
