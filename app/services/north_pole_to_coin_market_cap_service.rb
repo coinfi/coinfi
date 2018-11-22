a5f54854988eb0be68665858bf8dc10c722cd539
Han Chang <szu.han.chang@gmail.com>
2018-11-22 11:16:16 +0800

    Standardize NorthPole service, throw Rollbar warning

--- app/services/north_pole_to_coin_market_cap_service.rb
+++ app/services/north_pole_to_coin_market_cap_service.rb
@@ -1,24 +1,24 @@
-class NorthPoleToCoinMarketCapService
+class NorthPoleToCoinMarketCapService < Patterns::Service
   def initialize(north_pole_response)
     @body = JSON.parse(north_pole_response.body)
   end
 
   # Converts NorthPole proxy data format into the CoinMarketCap format.
-  def convert
+  def call
     if @body["markets"]
       market = @body["markets"][0]
       {
         "rank" => market.fetch("position", 0),
-        "price_usd" => market.dig("price", "usd") || 0,
-        "price_btc" => market.dig("price", "btc") || 0,
+        "price_usd" => market.dig("price", "usd") || 0.0,
+        "price_btc" => market.dig("price", "btc") || 0.0,
         "24h_volume_usd" => market.dig("volume24", "usd") || 0,
         "market_cap_usd" => market.dig("marketCap", "usd") || 0,
       }
     else # The slug passed in isn't valid on CMC...
       {
-        "rank" => "Unknown",
-        "price_usd" => 0,
-        "price_btc" => 0,
+        "rank" => "-",
+        "price_usd" => 0.0,
+        "price_btc" => 0.0,
         "24h_volume_usd" => 0,
         "market_cap_usd" => 0,
       }
