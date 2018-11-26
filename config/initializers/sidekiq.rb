5df832cb197ffe3172074cf871105ffc648ec197
Anthony Tsang <tsang@antho.com>
2018-11-26 10:12:56 +0800

    Configure sidekiq to use redis cloud.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,26 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
 
+is_production = (ENV['IS_PRODUCTION'] || "false").downcase == 'true'
+redis_url = ENV.fetch('REDISCLOUD_URL') if is_production
+
 Sidekiq.configure_server do |config|
+  if is_production
+    config.redis = { url: redis_url }
+  end
+
   config.on(:startup) do
     # load production-specific schedule config
-    is_production = ENV['IS_PRODUCTION'] || "false"
-    if is_production.downcase == 'true'
+    if is_production
       Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_production.yml', __FILE__))
       SidekiqScheduler::Scheduler.instance.reload_schedule!
     end
+  end
+end
+
+Sidekiq.configure_client do |config|
+  if is_production
+    config.redis = { url: redis_url }
   end
 end
 
