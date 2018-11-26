d6f6c291fbbcc868d270139135e0b990c7e4065e
Anthony Tsang <tsang@antho.com>
2018-11-26 09:37:49 +0800

    Change production check to rely on is_production env var.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,8 @@ require 'sidekiq-scheduler'
 Sidekiq.configure_server do |config|
   config.on(:startup) do
     # load production-specific schedule config
-    if ENV.fetch('ROOT_DOMAIN') == 'www.coinfi.com'
+    is_production = ENV['IS_PRODUCTION'] || "false"
+    if is_production.downcase == 'true'
       Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_production.yml', __FILE__))
       SidekiqScheduler::Scheduler.instance.reload_schedule!
     end
