59eec7f89b36aff8ca20fc20d2e042856bfe729c
Anthony Tsang <tsang@antho.com>
2018-11-27 11:25:29 +0800

    Componentize coins page banner.

--- app/javascript/App/bundles/HomeIndex/index.tsx
+++ app/javascript/App/bundles/HomeIndex/index.tsx
@@ -19,6 +19,7 @@ import TotalMarketCap, {
 } from '~/bundles/common/components/TotalMarketCap'
 import NewsList from '~/bundles/common/components/NewsList'
 import { CoinData } from './types'
+import Banner from './Banner'
 import Footer from './Footer'
 
 interface Props {
@@ -38,39 +39,6 @@ interface State {
 
 const styles = (theme) =>
   createStyles({
-    mainFeaturedPost: {
-      backgroundColor: '#253640',
-      color: '#d7d7d7',
-      boxShadow: 'none',
-    },
-    mainFeaturedPostContent: {
-      padding: `${theme.spacing.unit * 4}px`,
-      [theme.breakpoints.up('md')]: {
-        paddingLeft: 0,
-        paddingRight: 0,
-      },
-    },
-    header: {
-      marginBottom: '0.5em !important',
-      fontSize: '1.5rem',
-      textAlign: 'center',
-    },
-    subheader: {
-      fontSize: '0.8rem',
-      textAlign: 'center',
-      [theme.breakpoints.up('md')]: {
-        fontSize: '1rem',
-      },
-    },
-    cta: {
-      backgroundColor: '#23adf0',
-      color: '#fff',
-      borderRadius: '2px',
-      '&:hover': {
-        backgroundColor: '#23adf0',
-        boxShadow: '0 2px 10px rgba(14, 151, 255, 0.4)',
-      },
-    },
     widgetContainer: {
       backgroundColor: '#fff',
       [theme.breakpoints.up('md')]: {
@@ -138,43 +106,7 @@ class HomeIndex extends React.Component<Props, State> {
 
     return (
       <React.Fragment>
-        <Paper className={classes.mainFeaturedPost} square={true}>
-          <Grid
-            container={true}
-            direction="column"
-            justify="center"
-            alignItems="center"
-            className={classes.mainFeaturedPostContent}
-          >
-            <Grid item={true} md={true}>
-              <Typography className={classes.header} color="inherit">
-                Financial intelligence for cryptocurrency
-              </Typography>
-            </Grid>
-            <Grid item={true} md={true}>
-              <Typography
-                className={classes.subheader}
-                color="inherit"
-                paragraph={true}
-              >
-                Uncover buy and sell opportunities through data science backed
-                blockchain analytics.
-              </Typography>
-            </Grid>
-            <Grid item={true} md={true}>
-              <Button
-                className={classes.cta}
-                data-heap="homepage-click-signals-cta"
-                onClick={() => {
-                  window.location.href = `/signals`
-                }}
-              >
-                Learn more
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-
+        <Banner />
         <Grid
           container={true}
           direction="row"
