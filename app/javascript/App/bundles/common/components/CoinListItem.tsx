8b1666c3b70d60b5e7e0a9b8e0dadf4a16b48cc5
Anthony Tsang <tsang@antho.com>
2018-12-03 16:08:11 +0900

    Add currency symbol and rate information to currency context. Connect currency info to coinlist items.

--- app/javascript/App/bundles/common/components/CoinListItem.tsx
+++ app/javascript/App/bundles/common/components/CoinListItem.tsx
@@ -4,6 +4,9 @@ import classNames from 'classnames'
 import PercentageChange from '~/bundles/common/components/PercentageChange'
 import WatchStar from '~/bundles/common/components/WatchStar'
 import { Coin } from '~/bundles/common/types'
+import CurrencyContext, {
+  CurrencyContextType,
+} from '~/bundles/common/contexts/CurrencyContext'
 
 interface Props {
   coin: Coin
@@ -15,41 +18,52 @@ interface Props {
 const roundToDecimalPlaces = (num, places) =>
   Math.round(num * 10 ** places) / 10 ** places
 
-export default (props: Props) => {
-  const { coin, loggedIn } = props
+export default (props: Props) => (
+  <CurrencyContext.Consumer>
+    {({ currencyRate, currencySymbol }: CurrencyContextType) => {
+      const { coin, loggedIn } = props
 
-  const coinPrice = _.get(coin, ['market_info', 'price_usd'])
-  const coinPriceFixed = roundToDecimalPlaces(coinPrice, 4)
-  const percentChange = _.get(coin, ['market_info', 'change24h'])
+      const coinPrice =
+        parseFloat(_.get(coin, ['market_info', 'price_usd'])) * currencyRate
+      const coinPriceFixed = roundToDecimalPlaces(coinPrice, 4)
+      const percentChange = _.get(coin, ['market_info', 'change24h'])
 
-  return (
-    <a
-      href={`/news/${coin.slug}`}
-      className={classNames('pa-default b--b flex items-center pointer', {
-        'bg-foam': props.isSelected,
-      })}
-      style={{ minHeight: 57, color: '#555' }}
-      onClick={(event) => {
-        event.preventDefault()
-        props.onSelectCoin(coin)
-      }}
-    >
-      <WatchStar coin={coin} hasText={false} loggedIn={loggedIn} />
-      <div className="flex-auto flex justify-between items-center">
-        <div className="b f5 pl2">{coin.symbol}</div>
-        {coin.market_info && (
-          <div className="right-align">
-            {coinPrice ? (
-              <div>
-                $<span>{coinPriceFixed}</span>
+      return (
+        <a
+          href={`/news/${coin.slug}`}
+          className={classNames('pa-default b--b flex items-center pointer', {
+            'bg-foam': props.isSelected,
+          })}
+          style={{ minHeight: 57, color: '#555' }}
+          onClick={(event) => {
+            event.preventDefault()
+            props.onSelectCoin(coin)
+          }}
+        >
+          <WatchStar coin={coin} hasText={false} loggedIn={loggedIn} />
+          <div className="flex-auto flex justify-between items-center">
+            <div className="b f5 pl2">{coin.symbol}</div>
+            {coin.market_info && (
+              <div className="right-align">
+                {coinPrice ? (
+                  <>
+                    <div>
+                      {currencySymbol}
+                      <span>{coinPriceFixed}</span>
+                    </div>
+                    <PercentageChange
+                      value={percentChange}
+                      className="smaller2 b db"
+                    />
+                  </>
+                ) : (
+                  <div className="smaller3">UNLISTED</div>
+                )}
               </div>
-            ) : (
-              <div className="smaller3">UNLISTED</div>
             )}
-            <PercentageChange value={percentChange} className="smaller2 b db" />
           </div>
-        )}
-      </div>
-    </a>
-  )
-}
+        </a>
+      )
+    }}
+  </CurrencyContext.Consumer>
+)
--- app/javascript/App/bundles/common/contexts/CurrencyContext.tsx
+++ app/javascript/App/bundles/common/contexts/CurrencyContext.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 import * as _ from 'lodash'
 import { withCookies, Cookies } from 'react-cookie'
-import API from '~/bundles/common/utils/API'
+import API from '~/bundles/common/utils/localAPI'
+import currencyMap from '~/bundles/common/constants/currencyMap'
+import { STATUS_CODES } from 'http'
+
+enum STATUS {
+  INITIALIZING = 'INITIALIZING',
+  LOADING = 'LOADING',
+  READY = 'READY',
+}
 
 export interface CurrencyContextType {
   currency: string
+  currencyRate: number
+  currencySymbol: string
   changeCurrency: (currency: string) => void
 }
 
@@ -17,7 +27,13 @@ export interface CurrencyContextProps {
 }
 
 export interface CurrencyContextState {
+  status: string
+  currencies: {
+    [currencyKey: string]: number
+  }
   currency: string
+  currencySymbol: string
+  currencyRate: number
 }
 
 export const CURRENCY_CHANGE_EVENT = 'currencyChange'
@@ -32,24 +48,51 @@ class CurrencyProvider extends React.Component<
     const { cookies, user } = props
     const defaultCurrency = _.get(user, 'default_currency')
     const currency = defaultCurrency || cookies.get('currency') || 'USD'
+    const currencySymbol = _.get(currencyMap, currency, '')
+
     this.state = {
+      status: STATUS.INITIALIZING,
+      currencies: {},
       currency,
+      currencySymbol,
+      currencyRate: 1,
     }
   }
 
   public componentDidMount() {
     document.addEventListener(CURRENCY_CHANGE_EVENT, this.onCurrencyChange)
+    this.getCurrencyRates()
   }
 
   public componentDidUpdate(
     prevProps: CurrencyContextProps,
     prevState: CurrencyContextState,
   ) {
+    // update based on cookie
     const { cookies } = this.props
-    const currency = cookies.get('currency')
+    const cookieCurrency = cookies.get('currency')
 
-    if (prevState.currency !== currency) {
-      this.setState({ currency })
+    if (prevState.currency !== cookieCurrency) {
+      const currencyDetails = this.getCurrencyDetails(cookieCurrency)
+
+      this.setState({
+        currency: cookieCurrency,
+        ...currencyDetails,
+      })
+    }
+
+    // update if coming out of loading (i.e., got currency rate data)
+    if (
+      this.state.status === STATUS.READY &&
+      prevState.status !== this.state.status
+    ) {
+      const { currency } = this.state
+      const currencyDetails = this.getCurrencyDetails(currency)
+
+      this.setState({
+        currency,
+        ...currencyDetails,
+      })
     }
   }
 
@@ -57,10 +100,48 @@ class CurrencyProvider extends React.Component<
     document.removeEventListener(CURRENCY_CHANGE_EVENT, this.onCurrencyChange)
   }
 
+  public getCurrencyRates = () => {
+    const { status, currency } = this.state
+    if (status === STATUS.READY) {
+      this.setState({ status: STATUS.LOADING })
+    }
+
+    API.get('/currency').then((data) => {
+      const { payload } = data
+      if (_.isUndefined(payload)) {
+        return
+      }
+      const { updated_at, ...currencies } = payload
+      if (_.isUndefined(currencies)) {
+        return
+      }
+
+      this.setState({
+        status: STATUS.READY,
+        currencies,
+      })
+    })
+  }
+
+  public getCurrencyDetails(currency) {
+    const currencySymbol = _.get(currencyMap, currency, '')
+    const currencyRate = _.get(this.state.currencies, currency, 1)
+
+    return {
+      currencySymbol,
+      currencyRate,
+    }
+  }
+
   public onCurrencyChange = (e: CustomEvent) => {
     const { currency } = e.detail
+
+    const currencyDetails = this.getCurrencyDetails(currency)
     if (currency !== this.state.currency) {
-      this.setState({ currency })
+      this.setState({
+        currency,
+        ...currencyDetails,
+      })
     }
   }
 
@@ -68,10 +149,15 @@ class CurrencyProvider extends React.Component<
     const { cookies, loggedIn, user } = this.props
 
     cookies.set('currency', currency)
-    this.setState({ currency })
+    const currencyDetails = this.getCurrencyDetails(currency)
+
+    this.setState({
+      currency,
+      ...currencyDetails,
+    })
 
     if (loggedIn || user) {
-      API.patch('/user', { currency }, false)
+      API.patch('/user', { currency })
     }
 
     const event = new CustomEvent(CURRENCY_CHANGE_EVENT, {
@@ -83,6 +169,8 @@ class CurrencyProvider extends React.Component<
   public render() {
     const payload: CurrencyContextType = {
       currency: this.state.currency,
+      currencySymbol: this.state.currencySymbol,
+      currencyRate: this.state.currencyRate,
       changeCurrency: this.changeCurrency,
     }
 
