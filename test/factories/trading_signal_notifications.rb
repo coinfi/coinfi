98ee14a5664c0b0697d8e57cc8ce8a0d73cf642f
Jesse Hon <jesse@jessehon.com>
2018-12-04 10:27:07 +0800

    Fix `extra` attribute in factories

--- test/factories/trading_signal_notifications.rb
+++ test/factories/trading_signal_notifications.rb
@@ -7,5 +7,24 @@ FactoryBot.define do
     external_id { "0x#{Faker::Crypto.sha256}" }
     trading_signal_external_id { trading_signal ? trading_signal.external_id : "0x#{Faker::Crypto.sha256}" }
     timestamp { 1.minute.ago }
+
+    factory :telegram_trading_signal_notification do
+      transient do
+        telegram_chat_id { Faker::Number.number(9) }
+      end
+
+      extra {
+        {
+          "telegram": {
+            "message": {
+              "chat_id": telegram_chat_id,
+              "text": Faker::Markdown.emphasis,
+              "parse_mode": 'Markdown',
+              "disable_web_page_preview": true,
+            }
+          }
+        }
+      }
+    end
   end
 end
--- test/factories/trading_signals.rb
+++ test/factories/trading_signals.rb
@@ -7,24 +7,5 @@ FactoryBot.define do
     trading_signal_trigger_external_id { trading_signal_trigger ? trading_signal_trigger.external_id : "0x#{Faker::Crypto.sha256}" }
     timestamp { 5.minutes.ago }
     extra { {} }
-
-    factory :telegram_trading_signal do
-      transient do
-        telegram_chat_id { Faker::Number.number(9) }
-      end
-
-      extra {
-        {
-          "telegram": {
-            "message": {
-              "chat_id": telegram_chat_id,
-              "text": Faker::Markdown.emphasis,
-              "parse_mode": 'Markdown',
-              "disable_web_page_preview": true,
-            }
-          }
-        }
-      }
-    end
   end
 end
