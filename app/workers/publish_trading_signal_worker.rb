2c28b516c4814312fe1a1efa98e8e575e6057a25
Jesse Hon <jesse@jessehon.com>
2018-12-05 10:11:42 +0800

    Introduce PublishTradingSignalWorker to help manage pushing signals to Pub/Sub

---
+++ app/workers/publish_trading_signal_worker.rb
@@ -0,0 +1,19 @@
+require "google/cloud/pubsub"
+
+class PublishTradingSignalWorker
+  include Sidekiq::Worker
+
+  def perform(trading_signal)
+    pubsub = Google::Cloud::Pubsub.new(
+      project_id: ENV.fetch("GOOGLE_PROJECT_ID"),
+      credentials: JSON.parse(ENV.fetch("GOOGLE_CREDENTIALS_JSON"))
+    )
+
+    # Retrieve a topic
+    topic = pubsub.topic ENV.fetch("GOOGLE_PUBSUB_TRADING_SIGNALS_TOPIC_NAME")
+
+    # Publish a new pubsub message
+    pubsub_message_data = trading_signal.as_dto
+    pubsub_message = topic.publish(trading_signal.as_dto)
+  end
+end
