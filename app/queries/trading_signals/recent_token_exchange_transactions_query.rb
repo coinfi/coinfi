30ff52771352bd82e78daff21ec5db2330b28fa2
Jesse Hon <jesse@jessehon.com>
2018-12-05 16:43:53 +0800

    Fix comment and unnecessary variable assignment

--- app/queries/trading_signals/recent_token_exchange_transactions_query.rb
+++ app/queries/trading_signals/recent_token_exchange_transactions_query.rb
@@ -6,13 +6,11 @@ module TradingSignals
       relation = TradingSignal.all,
       time_window: DEFAULT_TIME_WINDOW
     )
-      # Until `trading_signal_triggers` table, we will need to use a workaround to find these
-      # `token-exchange-transactions` signals via its contents
-      result = relation
+      # Until `trading_signal_triggers` table is populated, we will need to use a workaround to find
+      # these `token-exchange-transactions` signals via its contents
+      relation
         .where("timestamp >= ?", DateTime.now - time_window)
         .where("(extra->>'token_transfer') IS NOT NULL")
-
-      result
     end
   end
 end
