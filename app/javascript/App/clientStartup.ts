9e652c515679097f24e9426a78a9376bdbad8846
Anthony Tsang <tsang@antho.com>
2018-12-12 11:30:29 +0900

    Revert "Try removing all other components."
    This reverts commit e6e3b060e9f3ed0a40d93604e888b732c50120f6.

--- app/javascript/App/clientStartup.ts
+++ app/javascript/App/clientStartup.ts
@@ -5,6 +5,10 @@ import withRootProviders from '~/withRootProviders'
 import ClientApp from './ClientApp'
 
 // non-react-router components
+import NavUserContainer from './bundles/common/containers/NavUserContainer'
+import CoinShow from './bundles/CoinShow'
+import FlashMessageListContainer from './bundles/common/containers/FlashMessageListContainer'
+import WatchStar from './bundles/common/components/WatchStar'
 import SignalExamplePanel from './bundles/signalsPage/SignalExamplePanel'
 import SignalFaqPanel from './bundles/signalsPage/SignalFaqPanel'
 import SignalPopoverText from './bundles/signalsPage/SignalPopoverText'
@@ -13,6 +17,9 @@ import SignalReservationForm from './bundles/signalsPage/SignalReservationForm'
 
 // Register the components so they can be rendered from Rails
 ReactOnRails.register({
+  NavUserContainer: withRootProviders(NavUserContainer),
+  FlashMessageListContainer: withRootProviders(FlashMessageListContainer),
+  WatchStar: withRootProviders(WatchStar),
   SignalExamplePanel: withRootProviders(SignalExamplePanel),
   SignalFaqPanel: withRootProviders(SignalFaqPanel),
   SignalPopoverText: withRootProviders(SignalPopoverText),
