5780d8487a40e134b5d0befdbc7c3892b8b25f1d
Jesse Hon <jesse@jessehon.com>
2018-12-19 14:49:38 +0800

    Create test for `bulk_upsert_by_external_id`

--- test/integration/api/signals/trading_signal_triggers_test.rb
+++ test/integration/api/signals/trading_signal_triggers_test.rb
@@ -27,4 +27,47 @@ class Api::Signals::TradingSignalTriggersTest < Api::Signals::BaseTest
     assert_equal 201, status
     assert_equal trading_signal_trigger_attrs.as_json, response.parsed_body.except("id")
   end
+
+  test "bulk upsert by external id" do
+    existing_trading_signal_triggers = create_list(:trading_signal_trigger, 5)
+
+    # Generate attributes for creates
+    new_trading_signal_triggers_attrs = attributes_for_list(:trading_signal_trigger, 5)
+    # Generate attributes for updates
+    updated_trading_signal_triggers_attrs = existing_trading_signal_triggers.map do |t|
+      t_attrs = t.attributes.symbolize_keys.except(:id, :updated_at, :created_at)
+      t_attrs.merge(attributes_for(:trading_signal_trigger).except(:external_id))
+    end
+
+    request_params_creates = new_trading_signal_triggers_attrs.index_by {|t_attrs| t_attrs.fetch(:external_id)}
+    request_params_updates = updated_trading_signal_triggers_attrs.index_by {|t_attrs| t_attrs.fetch(:external_id)}
+    request_params = {
+      trading_signal_triggers: request_params_creates.merge(request_params_updates)
+    }
+
+    assert_difference 'TradingSignalTrigger.count', 5 do
+      post "/api/signals/trading_signal_triggers/bulk_upsert_by_external_id",
+        params: request_params,
+        headers: auth_headers,
+        as: :json
+      assert_equal 200, status
+    end
+
+    # Check record creates are applied
+    new_trading_signal_triggers_attrs.each do |t_attrs|
+      assert_not_nil created_trading_signal_trigger = TradingSignalTrigger.find_by(external_id: t_attrs[:external_id])
+      assert_equal(
+        t_attrs.as_json,
+        created_trading_signal_trigger.attributes.except('id', 'updated_at', 'created_at')
+      )
+    end\
+    # Check record updates are applied
+    updated_trading_signal_triggers_attrs.each_with_index do |t_attrs, i|
+      existing_trading_signal_triggers[i].reload
+      assert_equal(
+        t_attrs.as_json,
+        existing_trading_signal_triggers[i].attributes.except('id', 'updated_at', 'created_at')
+      )
+    end
+  end
 end
