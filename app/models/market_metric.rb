21a68d54636fbcc7d13970f24de220d5a2ebacc7
Anthony Tsang <tsang@antho.com>
2019-01-04 15:53:46 +0900

    Remove where clause in case data is stale or slightly behind.

--- app/models/market_metric.rb
+++ app/models/market_metric.rb
@@ -4,7 +4,6 @@ class MarketMetric < ApplicationRecord
   scope :daily, -> (num_of_days = 30) {
     where(timestamp: MarketMetric
       .select('MAX(timestamp) as max_timestamp')
-      .where('timestamp >= ?', num_of_days.days.ago.beginning_of_day)
       .group('date(timestamp)')
       .order('max_timestamp DESC')
       .limit(num_of_days)
@@ -14,7 +13,6 @@ class MarketMetric < ApplicationRecord
   scope :monthly, -> (num_of_months = 12) {
     where(timestamp: MarketMetric
       .select('MAX(timestamp) as max_timestamp')
-      .where('timestamp >= ?', num_of_months.months.ago.beginning_of_day)
       .group('extract(year from timestamp)', 'extract(month from timestamp)')
       .order('max_timestamp DESC')
       .limit(num_of_months)
