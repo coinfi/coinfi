9bf8dd13d26c3c8825c2a1a4fbc4aac1b8fee4ff
Anthony Tsang <tsang@antho.com>
2019-02-07 18:05:38 +0900

    Add update exchange service.

--- app/services/coin_market_cap_pro/coin_market_cap_pro_helpers.rb
+++ app/services/coin_market_cap_pro/coin_market_cap_pro_helpers.rb
@@ -11,5 +11,41 @@ module CoinMarketCapPro
     def currency
       return "USD"
     end
+
+    def get_default_api_headers
+      @default_api_headers ||= { "X-CMC_PRO_API_KEY" => ENV.fetch('COINMARKETCAP_API_KEY') }
+    end
+
+    def extract_api_data(response, healthcheck_url = nil)
+      contents = JSON.parse(response.body)
+
+      # ping health check if api error
+      json_response_code = get_json_response_code(contents)
+
+      if response.success? && json_response_code == 0 then
+        return contents['data']
+      end
+
+      error_message = "ERROR HTTP(#{response.code}) JSON(#{json_response_code}): #{get_error_message(contents)}"
+      if healthcheck_url.present?
+        Net::HTTP.post(URI.parse("#{healthcheck_url}/fail"), error_message)
+      else
+        puts error_message
+      end
+
+      nil
+    end
+
+    def log_or_ping_on_missing_data(missing_data, healthcheck_url = nil)
+      if missing_data.empty?
+        Net::HTTP.get(URI.parse(healthcheck_url)) unless healthcheck_url.blank?
+      else
+        if healthcheck_url.present?
+          Net::HTTP.post(URI.parse("#{healthcheck_url}/fail"), missing_data.to_json)
+        else
+          pp missing_data
+        end
+      end
+    end
   end
 end
