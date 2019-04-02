c935def745e91aeb9c7a58dffe870e83588fef42
Anthony Tsang <tsang@antho.com>
2019-04-02 14:47:14 +0900

    Force toplist refresh when a top coin's snap pricing is updated

--- app/helpers/coin_list_helper.rb
+++ app/helpers/coin_list_helper.rb
@@ -1,12 +1,18 @@
 module CoinListHelper
-  def toplist_coins
-    distribute_reads(max_lag: ApplicationController::MAX_ACCEPTABLE_REPLICATION_LAG, lag_failover: true) do
-      serialized_coins = Rails.cache.fetch("coins/toplist", expires_in: 1.hour) do
-        coins = Coin.order(:ranking).limit(20)
+  TOP_LIST_LIMIT = 20
+
+  def toplist_coins(force_cache_refresh: false)
+    serialized_coins = Rails.cache.fetch("coins/toplist", expires_in: 1.hour, force: force_cache_refresh) do
+      distribute_reads(max_lag: ApplicationController::MAX_ACCEPTABLE_REPLICATION_LAG, lag_failover: true) do
+        coins = Coin.order(:ranking).limit(TOP_LIST_LIMIT)
         coinlist_serializer(coins)
       end
-      serialized_coins
     end
+    serialized_coins
+  end
+
+  def is_toplist_coin?(coin)
+    coin && coin.try(:ranking) <= TOP_LIST_LIMIT
   end
 
   def watchlist_coins
