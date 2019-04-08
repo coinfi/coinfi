6d93a534fa2533d8d215d36fc55bcdf93a9c5c92
anthotsang <tsang@antho.com>
2019-04-08 10:58:04 +0900

    Preconnect to required origins and increase static asset caching (#710)
    * Add preconnect for scripts
    
    * Increase cache length of static assets to 1 year
    
    * Increase public files to 1 year cache
    
    * Move up preconnect links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{1.year.to_i}"
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
