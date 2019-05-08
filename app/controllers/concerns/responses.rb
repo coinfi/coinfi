0a677fadae9965fcd29d6101977d7acffddc102f
anthotsang <tsang@antho.com>
2019-05-08 15:43:36 +0900

    Ignore expected 404 errors (#755)
    * Silence expected render 404 error
    
    * Silence expected news coin errors

--- app/controllers/concerns/responses.rb
+++ app/controllers/concerns/responses.rb
@@ -1,16 +1,18 @@
 module Responses
-  
+
   extend ActiveSupport::Concern
 
   included do
   end
 
   def render_404
-    raise ActionController::RoutingError.new('Not Found')
+    Rollbar.silenced {
+      raise ActionController::RoutingError.new('Not Found')
+    }
   end
 
   def render_403
     head :forbidden
   end
-  
+
 end
