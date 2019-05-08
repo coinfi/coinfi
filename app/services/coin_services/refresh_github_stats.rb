eece184d9596aab177c4aff6cd46dd0dacc53d4a
Anthony Tsang <tsang@antho.com>
2019-05-08 15:27:30 +0900

    Add additional checks for github data

--- app/services/coin_services/refresh_github_stats.rb
+++ app/services/coin_services/refresh_github_stats.rb
@@ -22,12 +22,38 @@ module CoinServices
     def refresh_github_stats(coins)
       failures = batch_process(coins) do |coin|
         result = coin.github_stats(force_refresh: true)
-        if result.nil? || result.empty?
-          raise StandardError, "No results for #{coin.slug}"
-        end
+        check_result(result, coin.slug)
       end
 
       @failed_updates += failures
+    end
+
+    def check_result(result, coin_slug)
+      if result.blank?
+        raise StandardError, "No results for #{coin_slug}"
+      elsif result[:commit_activity].blank?
+        raise StandardError, "No commit activity for #{coin_slug}"
+      elsif result[:code_frequency].blank?
+        raise StandardError, "No code frequency for #{coin_slug}"
+      elsif result[:snapshot].blank?
+        raise StandardError, "No snapshot for #{coin_slug}"
+      elsif !has_stats?(result[:snapshot])
+        raise StandardError, "Incomplete snapshot for #{coin_slug}"
+      end
+    end
+
+    def has_stats?(snapshot)
+      if snapshot[:watchers].blank?
+        false
+      elsif snapshot[:stargazers].blank?
+        false
+      elsif snapshot[:forks].blank?
+        false
+      elsif snapshot[:contributors].blank?
+        false
+      else
+        true
+      end
     end
   end
 end
