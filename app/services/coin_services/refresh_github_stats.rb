60970c306fd296914fa978ce8b401fb3da74a728
anthotsang <tsang@antho.com>
2019-06-04 11:01:00 +0900

    Move github stats data check to retrieval service (#766)

--- app/services/coin_services/refresh_github_stats.rb
+++ app/services/coin_services/refresh_github_stats.rb
@@ -43,35 +43,9 @@ module CoinServices
     def retrieve_coin_stats(coins)
       batch_process(coins) do |coin|
         result = coin.github_stats(force_refresh: true)
-        check_result(result, coin.slug)
-      end
-    end
-
-    def check_result(result, coin_slug)
-      if result.blank?
-        raise StandardError, "No results for #{coin_slug}"
-      elsif result[:commit_activity].blank?
-        raise StandardError, "No commit activity for #{coin_slug}"
-      elsif result[:code_frequency].blank?
-        raise StandardError, "No code frequency for #{coin_slug}"
-      elsif result[:snapshot].blank?
-        raise StandardError, "No snapshot for #{coin_slug}"
-      elsif !has_stats?(result[:snapshot])
-        raise StandardError, "Incomplete snapshot for #{coin_slug}"
-      end
-    end
-
-    def has_stats?(snapshot)
-      if snapshot[:watchers].blank?
-        false
-      elsif snapshot[:stargazers].blank?
-        false
-      elsif snapshot[:forks].blank?
-        false
-      elsif snapshot[:contributors].blank?
-        false
-      else
-        true
+        if result.blank?
+          raise StandardError, "No results for #{coin.slug}"
+        end
       end
     end
   end
