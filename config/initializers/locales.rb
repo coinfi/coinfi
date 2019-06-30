52c1b6206fbf02697566dc6d2c3e57f06fe8aecf
Anthony Tsang <tsang@antho.com>
2019-07-01 08:16:12 +0900

    Fix language codes

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,1 +1,1 @@
-I18n.available_locales = [:en, :es, :it, :de, :cn, :hk, :fr, :ar, :pl, :nl, :nb, :pt, :sw, :cs, :ml, :da, :ro, :th, :vi, :fi]
+I18n.available_locales = [:en, :fr, :'en-US', :'en-GB', :de, :es, :it, :ru, :ar, :'zh-CN', :'zh-TW', :pl, :pt, :nl, :nb, :sv, :cs, :da, :ms, :ro, :vi, :th, :fi]
