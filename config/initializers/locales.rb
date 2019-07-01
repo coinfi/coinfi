e7ccf9fdd758002d73402042201d8684d8e0b3f4
Anthony Tsang <tsang@antho.com>
2019-07-02 08:52:43 +0900

    Enable additional languages

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,1 +1,2 @@
-I18n.available_locales = [:en, :cn]
+I18n.available_locales = [:en, :fr, :'en-US', :'en-GB', :de, :es, :it, :ru, :ar, :'zh-CN', :'zh-TW', :pl, :pt, :nl, :nb, :sv, :cs, :da, :ms, :ro, :vi, :th, :fi]
+
