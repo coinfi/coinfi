b8210536187d93c0539944edf871c555238e4872
Anthony Tsang <tsang@antho.com>
2019-07-02 08:50:39 +0900

    Change query params for indicators

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = I18n.default_locale
   end
 
   def default_url_options
