220725dddd409166dc1f3f1d21b9068c2a370c6f
Anthony Tsang <tsang@antho.com>
2019-07-19 10:56:54 +0900

    Add middleware to remove invalid query string

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require_relative '../lib/middleware/handle_bad_encoding_middleware'
 
 require 'rails/all'
 
@@ -46,6 +47,8 @@ module CoinfiRails
     config.middleware.insert(0, Rack::Rewrite) do
       r301 %r{(.+)/$}, '$1'
     end
+
+    config.middleware.insert_before Rack::Runtime, HandleBadEncodingMiddleware
 
     # Give the asset pipeline access to npm packages
     config.assets.paths << Rails.root.join('node_modules')
