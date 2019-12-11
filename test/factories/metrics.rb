4bba3f4c1723db93dca7a1ef46ee9052f02f265e
Anthony Tsang <tsang@antho.com>
2019-12-11 13:06:09 +0900

    Fix syntax

--- test/factories/metrics.rb
+++ test/factories/metrics.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     token_address { token_address }
     metric_value { Faker::Number.decimal(2) }
     metric_type { metric_type }
-    sequence :date { |n| Date.today - n }
+    sequence(:date) { |n| Date.today - n }
   end
 end
