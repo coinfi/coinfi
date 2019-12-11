2817435404abc2751fb634c5ad0c5729d5a90d14
anthotsang <tsang@antho.com>
2019-12-12 08:18:54 +0900

    Upgrade ruby and sass (#850)
    * Change text styles
    
    * Manually hide some text for hopefully better seo results
    
    * Upgrade to ruby 2.5.1 and switch to sassc-rails
    
    * Revert heroku stack change
    
    * Fix syntax
    
    * Update ruby minor version and try to improve review app db copy

--- test/factories/market_metrics.rb
+++ test/factories/market_metrics.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :market_metric do
     total_market_cap { Faker::Number.decimal(12, 2) }
     total_volume_24h { Faker::Number.decimal(11, 2) }
-    sequence :timestamp { |n| Faker::Time.backward(n) }
+    sequence(:timestamp) { |n| Faker::Time.backward(n) }
   end
 end
