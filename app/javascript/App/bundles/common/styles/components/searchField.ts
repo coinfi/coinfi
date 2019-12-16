4e05b9990a1a276267635193a1e6193785889dbc
Anthony Tsang <tsang@antho.com>
2019-12-16 11:24:25 +0900

    Remove unfinished styles

--- app/javascript/App/bundles/common/styles/components/searchField.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from '@material-ui/core/styles/withStyles'
-
-const searchFieldStyle = (theme) => {
-  const isDarkMode = theme.palette.type === 'dark'
-
-  return {} as CSSProperties
-}
-
-export default searchFieldStyle
