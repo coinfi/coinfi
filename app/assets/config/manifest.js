39d458b1c8b371e5d0f4e4459bddaea408197cff
Anthony Tsang <tsang@antho.com>
2019-12-25 10:27:26 +0900

    Fix administrate resources

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,3 +5,10 @@
 //= link calculators.css
 //= link icos.css
 //= link indicators.css
+
+//= link administrate/application.js
+//= link administrate-field-json/application.js
+//= link administrate-field-nested_has_many/application.js
+//= link administrate/application.css
+//= link administrate-field-json/application.css
+//= link administrate-field-nested_has_many/application.css
