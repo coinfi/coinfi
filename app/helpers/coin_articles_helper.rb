2f501586d1ce30d8bdd752a5d0a6a385bc5cb170
Anthony Tsang <tsang@antho.com>
2019-12-26 16:23:28 +0900

    Show publish or last updated date for coin articles

--- app/helpers/coin_articles_helper.rb
+++ app/helpers/coin_articles_helper.rb
@@ -22,4 +22,18 @@ module CoinArticlesHelper
     @markdown_toc ||= Redcarpet::Markdown.new(@markdown_toc_renderer)
     @markdown_toc.render(data).html_safe
   end
+
+  def display_article_date(article)
+    return "" unless article.present?
+
+    if article.created_at == article.updated_at
+      "Published: #{get_formatted_date(article.created_at)}"
+    else
+      "Last Updated: #{get_formatted_date(article.updated_at)}"
+    end
+  end
+
+  def get_formatted_date(date)
+    date.strftime('%B %-d, %Y')
+  end
 end
