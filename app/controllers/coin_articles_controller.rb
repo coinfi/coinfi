b8d42f88f107a0527c147ec422be3896e2b64061
Anthony Tsang <tsang@antho.com>
2019-12-26 10:10:18 +0900

    Implement coin articles index page with pagination

--- app/controllers/coin_articles_controller.rb
+++ app/controllers/coin_articles_controller.rb
@@ -1,25 +1,34 @@
 class CoinArticlesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   before_action :set_coin_article, only: [:show]
 
   def index
-    @coin_articles = CoinArticle.all
+    distribute_reads(max_lag: MAX_ACCEPTABLE_REPLICATION_LAG, lag_failover: true) do
+      @coin_articles = CoinArticle.page(params[:page]).per(params[:limit])
+    end
+
   end
 
   def show
-    @title = @coin_article.meta_title || @coin_article.title
-
     set_meta_tags(
-      title: @title,
+      title: @coin_article.display_title,
       description: @coin_article.meta_description
     )
   end
 
   private
-    def set_coin_article
+
+  def set_coin_article
+    distribute_reads(max_lag: MAX_ACCEPTABLE_REPLICATION_LAG, lag_failover: true) do
       @coin_article = CoinArticle.friendly.find(params[:id])
     end
+  end
 
-    def coin_article_params
-      params.fetch(:coin_article, {})
-    end
+  def coin_article_params
+    params.fetch(:coin_article, {})
+  end
+
+  def record_not_found
+    render_404
+  end
 end
--- app/models/coin_article.rb
+++ app/models/coin_article.rb
@@ -4,6 +4,12 @@ class CoinArticle < ApplicationRecord
   belongs_to :coin
   belongs_to :author
   validates :title, presence: true
+  paginates_per 100
+  max_paginates_per 100
+
+  def display_title
+    meta_title || title
+  end
 
   private
 
