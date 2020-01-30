dbf9666436464e97104aa137a896291bfe8cd571
Anthony Tsang <tsang@antho.com>
2020-01-30 15:09:43 +0900

    Fix precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( ag-grid.css calculators.css coin-articles.css icos.css indicators.css )
+Rails.application.config.assets.precompile += %w(
+  ag-grid.css
+  calculators.css
+  coin-articles.css
+  icos.css
+  indicators.css
+  amp-application.css
+  modules/amp-coin-articles-page.css
+)
