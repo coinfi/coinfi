3ce83a85c066e7e11199e145de3ba1a6b00805a0
anthotsang <tsang@antho.com>
2020-01-31 11:40:24 +0900

    Amp pages (#883)
    * Add basic amp settings and move rollbar-js to only appear on non-amp pages
    
    * Add amp page to coin article index
    
    * Eager load distributed, paginated call
    
    * Fix precompiled assets
    
    * Add missing route changes
    
    * Add missing account icons
    
    * Adjust sidebar styles
    
    * Remove url-encoded characters

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,11 @@ Rails.application.routes.draw do
 
   resources :author_profiles, only: %i[index show create update], path: 'authors'
 
+  # HTML+AMP routes
+  scope constraints: lambda { |req| req.format == :html || req.format == :amp } do
+    resources :coin_articles, only: %i[index], path: 'how-to-buy-cryptocurrency'
+  end
+
   # HTML only routes
   scope defaults: { format: 'html' }, constraints: { format: 'html' } do
     get '/calculators/:id', to: 'calculators#show', as: 'calculator'
@@ -41,7 +46,7 @@ Rails.application.routes.draw do
     resources :contributor_submissions, path: 'contributor-submissions'
     resources :exchange_listings, only: :index, path: 'listings'
     resources :calendar_events, only: :index, path: 'calendar'
-    resources :coin_articles, only: %i[index show], path: 'how-to-buy-cryptocurrency'
+    resources :coin_articles, only: %i[show], path: 'how-to-buy-cryptocurrency'
     get '/indicators', to: 'indicators#show', as: 'indicators'
     # get '/icos', to: redirect('/icos/upcoming'), as: 'icos_root'
     # get '/icos(/:status)', to: 'icos#index', as: 'icos'
@@ -49,7 +54,6 @@ Rails.application.routes.draw do
     get '/news/:id/:slug', to: 'news#show', as: 'news_item'
     get '/news/:coin_slug', to: 'news#coin_index', as: 'news_coin'
     get '/news', to: 'news#index'
-    get '/news-beta', to: static('/news-beta.html')
     get '/podcast', to: redirect('https://www.coinfi.com/research/coinfi-podcast', status: 302), as: 'podcast'
     get '/profile', to: 'users#edit'
     put '/profile', to: 'users#update'
