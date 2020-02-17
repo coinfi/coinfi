b481eb198e72e33fc087844848dcca24584b5b8f
anthotsang <tsang@antho.com>
2020-02-17 16:31:56 +0900

    Add how to buy cardano page amp page (#893)

--- app/helpers/coin_articles_helper.rb
+++ app/helpers/coin_articles_helper.rb
@@ -24,6 +24,66 @@ module CoinArticlesHelper
     @markdown_toc.render(sanitized_data).html_safe
   end
 
+  def amp_markdown(data)
+    raw_html = markdown data
+    parsed_html = Nokogiri::HTML::DocumentFragment.parse(raw_html)
+
+    # Group nodes into accordion sections based on h2 tags
+    stack = []
+    current_container = nil
+    parsed_html.children.each do |node|
+      if node.name == "h2"
+        section = Nokogiri::XML::Node.new "section", parsed_html
+        section["id"] = "#{node.text.parameterize}-section"
+        stack << section
+        section.add_child node
+
+        # amp accordion can only have two children (i.e., header/content)
+        # all content must be children of the content child
+        current_container = Nokogiri::XML::Node.new "div", parsed_html
+        section.add_child current_container
+      else
+        # Handle case where there are nodes before the first H2
+        # This section will be outside the accordion
+        if current_container.blank?
+          current_container = Nokogiri::XML::Node.new "section", parsed_html
+          current_container["id"] = "top-section"
+          current_container.add_class "p2"
+          parsed_html.prepend_child current_container
+        end
+
+        current_container.add_child node
+      end
+    end
+
+    accordion = Nokogiri::XML::Node.new "amp-accordion", parsed_html
+    accordion["id"] = "content"
+    accordion.set_attribute "expand-single-section", ""
+    stack.each do |section|
+      accordion.add_child section
+    end
+
+    parsed_html.add_child accordion
+    # END grouping into accordion
+
+    parsed_html.to_html.html_safe
+  end
+
+  def amp_markdown_toc(data)
+    raw_html = markdown_toc data
+    parsed_html = Nokogiri::HTML::DocumentFragment.parse(raw_html)
+    parsed_html.xpath("ul/li").each do |li|
+      anchor_nodes = li.css("a[href^='#']")
+      anchor_text = anchor_nodes.first["href"].gsub("#", "")
+
+      anchor_nodes.each do |node|
+        node.set_attribute "on", "tap:content.expand(section=#{anchor_text}-section)"
+      end
+    end
+
+    parsed_html.to_html.html_safe
+  end
+
   def sanitize_html(raw_html)
     @html_sanitizer ||= Rails::Html::SafeListSanitizer.new
     @html_scrubber ||= Scrubbers::CoinArticleScrubber.new
