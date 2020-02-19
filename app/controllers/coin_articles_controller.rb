f659692b0e077842c75448f9bc054c8ef72d56f1
Anthony Tsang <tsang@antho.com>
2020-02-19 09:40:16 +0900

    Include all required article schema for amp pages

--- app/controllers/coin_articles_controller.rb
+++ app/controllers/coin_articles_controller.rb
@@ -21,9 +21,27 @@ class CoinArticlesController < ApplicationController
       title: @coin_article.meta_title || @coin_article.title,
       description: @coin_article.meta_description
     )
-    set_jsonld({
-      "@context": "http://schema.org/",
-    }.merge(@coin_article.get_schema))
+    set_jsonld(
+      {
+        "@context": "http://schema.org/",
+        "publisher": {
+          "@type": "Organization",
+          "name": "CoinFi",
+          "url": "https://#{ENV.fetch('ROOT_DOMAIN')}",
+          "logo": {
+            "@type": "ImageObject",
+            "url": view_context.image_url('amp-logo.png'),
+            "height": "174",
+            "width": "60"
+          }
+        },
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": url_for(format: :html, only_path: false),
+        },
+        "image": view_context.image_url('amp-logo.png'), # default image
+      }.merge(@coin_article.get_schema)
+    )
   end
 
   private
