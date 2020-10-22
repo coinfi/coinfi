e43e2f930b683a4d74f70e1ecb5534b8307cd822
anthotsang <tsang@antho.com>
2020-10-22 17:03:01 +0900

    Allow white-listed domains for coin article markdown rendering (#933)

--- lib/renderers/coin_article_renderer.rb
+++ lib/renderers/coin_article_renderer.rb
@@ -23,14 +23,29 @@ module Renderers
     private
 
     def is_external_link?(link)
-      true unless ENV['ROOT_DOMAIN'].present?
+      link_host = get_host_from_url(link)
+      link_host.present? && !is_whitelisted_host?(link_host)
+    end
 
-      link_uri = URI.parse(link)
-      link_host = link_uri.host
-      if link_host.present? && link_uri.port.present?
-        link_host = "#{link_host}:#{link_uri.port}"
-      end
-      link_host.present? && ENV['ROOT_DOMAIN'] != link_host
+    def get_host_from_url(link)
+      uri = URI.parse(link)
+      host = uri.host
+      host = "#{host}:#{uri.port}" if host.present? && uri.port.present? && uri.port != uri.default_port
+
+      host.strip
+    end
+
+    def is_whitelisted_host?(host)
+      host = host.downcase
+      # host matches domain exactly or matches subdomain
+      whitelisted_domains.any? {|domain| host == domain || host.rindex('.' + domain) != nil }
+    end
+
+    def whitelisted_domains
+      @whitelisted_domains ||= [
+        ENV['ROOT_DOMAIN'],
+        *ENV.fetch('WHITELISTED_DOMAINS'){ '' }.split(',').map(&:strip)
+      ].map(&:downcase)
     end
   end
 end
