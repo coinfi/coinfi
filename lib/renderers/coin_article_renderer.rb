76f8fe0b29773b84f1dd8de9a6fe075179169b59
Anthony Tsang <tsang@antho.com>
2022-02-28 15:00:52 +0900

    Remove rel attributes for external links in coin articles

--- lib/renderers/coin_article_renderer.rb
+++ lib/renderers/coin_article_renderer.rb
@@ -10,42 +10,11 @@ module Renderers
       if title.present?
         additional_attrs += " title=\"#{title}\""
       end
-      if is_external_link?(link)
-        additional_attrs += " target=\"_blank\" rel=\"nofollow noopener noreferrer\""
-      end
       if @options.key?(:link_attributes)
         additional_attrs += " " + @options[:link_attributes].map{|k, v| " #{k}=\"#{v}\""}.join(' ')
       end
 
       "<a href=\"#{link}\"#{additional_attrs}>#{content}</a>"
-    end
-
-    private
-
-    def is_external_link?(link)
-      link_host = get_host_from_url(link)
-      link_host.present? && !is_whitelisted_host?(link_host)
-    end
-
-    def get_host_from_url(link)
-      uri = URI.parse(link)
-      host = uri.host
-      host = "#{host}:#{uri.port}" if host.present? && uri.port.present? && uri.port != uri.default_port
-
-      host.respond_to?(:strip) ? host.strip : host
-    end
-
-    def is_whitelisted_host?(host)
-      host = host.downcase
-      # host matches domain exactly or matches subdomain
-      whitelisted_domains.any? {|domain| host == domain || host.rindex('.' + domain) != nil }
-    end
-
-    def whitelisted_domains
-      @whitelisted_domains ||= [
-        ENV['ROOT_DOMAIN'],
-        *ENV.fetch('WHITELISTED_DOMAINS'){ '' }.split(',').map(&:strip)
-      ].map(&:downcase)
     end
   end
 end
