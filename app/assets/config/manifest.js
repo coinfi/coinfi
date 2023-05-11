fd473fd85e855a45a65a711be1606601fa050a4c
Anthony Tsang <tsang@antho.com>
2023-05-11 16:32:50 +0900

    Update administrate assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,8 +4,8 @@
 // Administrate
 //= link administrate/application.js
 //= link administrate/application.css
-//= link administrate-field-json/application.js
-//= link administrate-field-json/application.css
+//= link administrate-field-jsonb/application.js
+//= link administrate-field-jsonb/application.css
 //= link administrate-field-nested_has_many/application.js
 //= link administrate-field-nested_has_many/application.css
 //
