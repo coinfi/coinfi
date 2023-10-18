9f7426aa154c443690db39c79e68b90341395af6
Anthony Tsang <tsang@antho.com>
2023-10-18 11:09:23 +0900

    Remove COINFI_POSTGREST_URL reliance and use local data instead. Remove unused hourly_prices_data. Reschedule cmc daily prices to better align with daily_prices refresh timing. Put off refactoring indicator calculations to rely directly on daily_ohcl_prices rather than prices_data

--- test/application_integration_test.rb
+++ test/application_integration_test.rb
@@ -3,17 +3,6 @@ require "test_helper"
 class ApplicationIntegrationTest < ActionDispatch::IntegrationTest
   setup do
     ReactOnRails::TestHelper.ensure_assets_compiled
-
-    postgres_url = ENV.fetch('COINFI_POSTGREST_URL')
-
-    stub_request(:get, Regexp.new("#{postgres_url}/daily_ohcl_prices")).
-      to_return(status: 200, body: File.new(Rails.root.join('test', 'fixtures', 'daily_ohcl_prices.json')))
-
-    stub_request(:get, Regexp.new("#{postgres_url}/cmc_daily_ohcl_prices")).
-      to_return(status: 200, body: File.new(Rails.root.join('test', 'fixtures', 'cmc_daily_ohcl_prices.json')))
-
-    stub_request(:get, Regexp.new("#{postgres_url}/hourly_ohcl_prices")).
-      to_return(status: 200, body: File.new(Rails.root.join('test', 'fixtures', 'hourly_ohcl_prices.json')))
   end
 
   protected
