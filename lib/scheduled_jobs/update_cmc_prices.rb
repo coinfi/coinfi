2348ecca273bcb4cdaa806ac5c1fb58f086950e6
Anthony Tsang <tsang@antho.com>
2023-12-06 14:01:49 -0800

    Reduce total credits spent for updating price snapshots

--- lib/scheduled_jobs/update_cmc_prices.rb
+++ lib/scheduled_jobs/update_cmc_prices.rb
@@ -5,27 +5,39 @@ class UpdateCmcPrices
   # This makes up to 5 API calls per minute
   def perform(metadata)
     minutes = Time.at(metadata["scheduled_at"]).min
-
-    (1..5).each do |number_of_minutes|
-      is_number_of_minutes = minutes % number_of_minutes == 0
-      if is_number_of_minutes
-        update_prices(number_of_minutes)
-      end
-    end
+    update_prices(minutes)
   end
 
-  def update_prices(number_of_minutes)
-    case number_of_minutes
-    when 1 # Top 1-100
+  # 8,064 credits/day
+  # 1,440 credits (1 credit/minute)
+  # 1,440 credits (2 credits/2 minutes)
+  # 1,440 credits (3 credits/3 minutes)
+  # 1,440 (5 credits/5 minutes)
+  # 2,304 (~24 credits/15 minutes)
+  def update_prices(minutes)
+    # Top 1-100
+    if minutes % 1 == 0 # Redundant, but done for uniformity
       CoinMarketCapPro::UpdateTickerService.call(start: 1, limit: 100, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES1'))
-    when 2 # Top 101-200
-      CoinMarketCapPro::UpdateTickerService.call(start: 101, limit: 100, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES2'))
-    when 3 # Top 201-500
-      CoinMarketCapPro::UpdateTickerService.call(start: 201, limit: 300, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES3'))
-    when 4 # Top 501-1000
-      CoinMarketCapPro::UpdateTickerService.call(start: 501, limit: 500, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES4'))
-    when 5 # Top 1001+
-      CoinMarketCapPro::UpdateTickerService.call(start: 1001, limit: 5000, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES5')) # 5000 is CMC's max limit
+    end
+
+    # Top 101-300
+    if minutes % 2 == 0
+      CoinMarketCapPro::UpdateTickerService.call(start: 101, limit: 200, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES2'))
+    end
+
+    # Top 301-600
+    if minutes % 3 == 0
+      CoinMarketCapPro::UpdateTickerService.call(start: 301, limit: 300, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES3'))
+    end
+
+    # Top 601-1100
+    if minutes % 5 == 0
+      CoinMarketCapPro::UpdateTickerService.call(start: 601, limit: 500, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES4'))
+    end
+
+    # Top 1101+
+    if minutes % 15 == 0
+      CoinMarketCapPro::UpdateTickerService.call(start: 1101, limit: 5000, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES5')) # 5000 is CMC's max limit
     end
   end
 end
