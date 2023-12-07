336f056c012466a48cf345f25783a32687f2abbb
Anthony Tsang <tsang@antho.com>
2023-12-06 16:07:18 -0800

    Update price snapshot frequency (it's 200 coins/credit and it's based on the limit rather than the amount returned). Also refactor to avoid having to calculate start every time values are changed

--- lib/scheduled_jobs/update_cmc_prices.rb
+++ lib/scheduled_jobs/update_cmc_prices.rb
@@ -8,36 +8,48 @@ class UpdateCmcPrices
     update_prices(minutes)
   end
 
-  # 8,064 credits/day
-  # 1,440 credits (1 credit/minute)
-  # 1,440 credits (2 credits/2 minutes)
-  # 1,440 credits (3 credits/3 minutes)
-  # 1,440 (5 credits/5 minutes)
-  # 2,304 (~24 credits/15 minutes)
   def update_prices(minutes)
-    # Top 1-100
-    if minutes % 1 == 0 # Redundant, but done for uniformity
-      CoinMarketCapPro::UpdateTickerService.call(start: 1, limit: 100, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES1'))
+    start = 1
+    updates_to_perform.each.with_index(1) do |update, index|
+      if minutes % update[:minutes] == 0
+        CoinMarketCapPro::UpdateTickerService.call(
+          start: start,
+          limit: update[:coins],
+          healthcheck_url: ENV.fetch("HEALTHCHECK_SNAP_PRICES#{index}")
+        )
+      end
+      start += update[:coins]
     end
+  end
 
-    # Top 101-300
-    if minutes % 2 == 0
-      CoinMarketCapPro::UpdateTickerService.call(start: 101, limit: 200, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES2'))
-    end
-
-    # Top 301-600
-    if minutes % 3 == 0
-      CoinMarketCapPro::UpdateTickerService.call(start: 301, limit: 300, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES3'))
-    end
-
-    # Top 601-1100
-    if minutes % 5 == 0
-      CoinMarketCapPro::UpdateTickerService.call(start: 601, limit: 500, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES4'))
-    end
-
-    # Top 1101+
-    if minutes % 15 == 0
-      CoinMarketCapPro::UpdateTickerService.call(start: 1101, limit: 5000, healthcheck_url: ENV.fetch('HEALTHCHECK_SNAP_PRICES5')) # 5000 is CMC's max limit
-    end
+  # 7,584 credits/day
+  # 1,440 credits (1 credit/minute)
+  # 720 credits (1 credits/2 minutes)
+  # 960 credits (2 credits/3 minutes)
+  # 864 (3 credits/5 minutes)
+  # 3,600 (25 credits/10 minutes)
+  def updates_to_perform
+    [
+      {
+        minutes: 1,
+        coins: 200,
+      },
+      {
+        minutes: 2,
+        coins: 200,
+      },
+      {
+        minutes: 3,
+        coins: 400,
+      },
+      {
+        minutes: 5,
+        coins: 600,
+      },
+      {
+        minutes: 10,
+        coins: 5000,
+      },
+    ]
   end
 end
